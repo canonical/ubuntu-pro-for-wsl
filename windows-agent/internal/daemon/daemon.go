63517a610bd43b60b79a404a85465056eadb0f20
Didier Roche <didier.roche@canonical.com>
2023-01-24 11:40:34 +0100

    Daemon manager to handle Windows service manager
    This deamon is compatible with the Windows service API and allow to
    register some GRPC services.
    It listens on a random tcp port.

--- windows-agent/internal/daemon/daemon.go
+++ windows-agent/internal/daemon/daemon.go
@@ -1,75 +1,163 @@
 package daemon
 
 import (
+	"context"
 	"net"
-	"os"
 
 	"google.golang.org/grpc"
 
-	agentAPI "github.com/canonical/ubuntu-pro-for-windows/agent-api"
 	"github.com/canonical/ubuntu-pro-for-windows/windows-agent/internal/decorate"
+	log "github.com/canonical/ubuntu-pro-for-windows/windows-agent/internal/grpc/logstreamer"
+	"github.com/canonical/ubuntu-pro-for-windows/windows-agent/internal/i18n"
+	"github.com/canonical/ubuntu-pro-for-windows/windows-agent/internal/loghooks"
 	"github.com/kardianos/service"
 )
 
-// GRPCServerRegisterer is a function that the daemon will call once to register all associated GRPC servers.
-type GRPCServerRegisterer func(srv *Daemon) *grpc.Server
+// GRPCServiceRegisterer is a function that the daemon will call everytime we want to build a new GRPC object.
+type GRPCServiceRegisterer func(ctx context.Context) *grpc.Server
 
 // Daemon is a daemon for windows agents with grpc support
 type Daemon struct {
-	lis                net.Listener
-	registerGRPCServer GRPCServerRegisterer
+	ctx context.Context
 
-	grpcServer grpc.Server
+	listener   listener
+	winService service.Service
 }
 
 // New returns an new, initialized daemon server that is ready to register GRPC services.
 // It hooks up to windows service management handler.
-func New(registerGRPCServer GRPCServerRegisterer, grpcPort uint) (d *Daemon, err error) {
-	defer decorate.OnError(&err /*i18n.G(*/, "can't create daemon" /*)*/)
+func New(ctx context.Context, registerGRPCServices GRPCServiceRegisterer) (d Daemon, err error) {
+	defer decorate.OnError(&err, i18n.G("can't create daemon"))
 
-	os.File
+	log.Debug(ctx, "Building new daemon")
+
 	// FIXME: To look at: https://learn.microsoft.com/en-us/windows/win32/services/interactive-services
+
+	config := service.Config{
+		Name:        "ubuntu-pro-agent",
+		DisplayName: "Ubuntu Pro Agent",
+		Description: "Monitors and manage Ubuntu WSL on your system",
+	}
+
+	listener := listener{
+		grpcServer: registerGRPCServices(ctx),
+	}
+
+	s, err := service.New(&listener, &config)
+	if err != nil {
+		return d, err
+	}
 
 	// If we're not running in interactive mode (CLI), add a hook to the logger
 	// so that the service can log to the Windows Event Log.
 	if !service.Interactive() {
 		logger, err := s.Logger(nil)
 		if err != nil {
-			return nil, err
+			return d, err
 		}
-		//log.AddHook(ctx, &loghooks.EventLog{Logger: logger})
+		log.AddHook(ctx, &loghooks.EventLog{Logger: logger})
 	}
 
-	/*
-		Write a file on disk for the selected port.
+	return Daemon{
+		ctx: ctx,
 
-		LISTEN SHOULD ONLY BE ON START
-		lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", grpcPort))
-		if err != nil {
-			return nil, fmt.Errorf("can't listen on port %d: %v", grpcPort, err)
-		}
-	*/
+		listener:   listener,
+		winService: s,
+	}, nil
+}
 
-	d := &Daemon{
-		lis:                lis,
-		registerGRPCServer: registerGRPCServer,
+// RunAsService runs as a windows service.
+func (d Daemon) RunAsService() (err error) {
+	defer decorate.OnError(&err, i18n.G("error while running as service"))
+
+	log.Debug(d.ctx, "Run daemon as a service")
+
+	return d.winService.Run()
+}
+
+// Run runs syncrhonously, skipping the windows service management part.
+func (d Daemon) Run() (err error) {
+	defer decorate.OnError(&err, i18n.G("error while running"))
+
+	log.Debug(d.ctx, "Run daemon synchronously")
+
+	lis, err := d.listener.listen()
+	if err != nil {
+		return err
+	}
+	return d.listener.serve(lis)
+}
+
+// Quit gracefully quits listening loop and stops the grpc server.
+// It can drops any existing connexion is force is true.
+func (d Daemon) Quit(force bool) {
+	log.Info(d.ctx, "Stopping daemon requested.")
+	if force {
+		d.listener.grpcServer.Stop()
+		return
 	}
 
-	config := service.Config{
-		Name:        "ubuntu-pro-agent",
-		DisplayName: "Ubuntu Pro Agent",
-		Description: "Monitors and manage Ubuntu WSL on your system",
-		Option:      service.KeyValue,
+	log.Info(d.ctx, i18n.G("Wait for active requests to close."))
+	d.listener.grpcServer.GracefulStop()
+	log.Debug(d.ctx, i18n.G("All connections have now ended."))
+}
+
+// listener is the internal object which actually deal with socket/GRPC and implements the windows service manager API.
+type listener struct {
+	ctx context.Context
+
+	grpcServer *grpc.Server
+	errs       chan error
+}
+
+// Start will start listening and server GRPC requests from the windows service manager.
+func (l *listener) Start(s service.Service) (err error) {
+	defer decorate.OnError(&err, i18n.G("error while starting service"))
+
+	lis, err := l.listen()
+	if err != nil {
+		return err
 	}
-	s, err := service.New(d, &config)
+
+	l.errs = make(chan error)
+	go func() {
+		l.errs <- l.serve(lis)
+	}()
+
+	return nil
+}
+
+// Stop will stop server GRPC requests from the windows service manager.
+func (l *listener) Stop(s service.Service) (err error) {
+	defer decorate.OnError(&err, i18n.G("error while stopping service"))
+
+	l.grpcServer.GracefulStop()
+
+	// Once we are done, return any error from the GRPC server
+	return <-l.errs
+}
+
+// listen returns a free tcp socket to listen on.
+func (l listener) listen() (lis net.Listener, err error) {
+	defer decorate.OnError(&err, i18n.G("can't listen"))
+
+	lis, err = net.Listen("tcp", "")
 	if err != nil {
 		return nil, err
 	}
 
-	return d, nil
+	return lis, nil
 }
 
-func Run() {
-	grpcServer := grpc.NewServer()
-	agentAPI.RegisterUIServer(grpcServer)
+// serve serves the grpc server on the listener.
+// It writes before a file on disk on which port it’s listening on for client.
+// This file is removed once the server stop listening.
+func (l listener) serve(lis net.Listener) (err error) {
+	addr := lis.Addr().String()
+	defer decorate.OnError(&err, i18n.G("error while serving on %s"), addr)
+
+	log.Infof(l.ctx, "Serving GRPC requests on %v", addr)
+
+	// TODO: Write a file on disk for the selected port.
+	return l.grpcServer.Serve(lis)
 }
