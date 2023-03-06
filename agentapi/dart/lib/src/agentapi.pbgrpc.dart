84d87342ce4cf292e6a8c7965b505fc3e4d1958f
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-03-06 11:12:27 -0300

    Wraps gRPC Dart code in a library
    Useful to avoid importing gRPC classes with lots of `../../..` in client code.
    Also, with that approach we can control what gets exported.
    We can exclude nasty implementation details.
    Library is minimal: no examples, no tests no docs. Just the essential
    code.

---
+++ agentapi/dart/lib/src/agentapi.pbgrpc.dart
@@ -0,0 +1,86 @@
+///
+//  Generated code. Do not modify.
+//  source: agentapi.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:async' as $async;
+
+import 'dart:core' as $core;
+
+import 'package:grpc/service_api.dart' as $grpc;
+import 'agentapi.pb.dart' as $0;
+export 'agentapi.pb.dart';
+
+class UIClient extends $grpc.Client {
+  static final _$ping = $grpc.ClientMethod<$0.Empty, $0.Empty>(
+      '/agentapi.UI/Ping',
+      ($0.Empty value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $0.Empty.fromBuffer(value));
+
+  UIClient($grpc.ClientChannel channel,
+      {$grpc.CallOptions? options,
+      $core.Iterable<$grpc.ClientInterceptor>? interceptors})
+      : super(channel, options: options, interceptors: interceptors);
+
+  $grpc.ResponseFuture<$0.Empty> ping($0.Empty request,
+      {$grpc.CallOptions? options}) {
+    return $createUnaryCall(_$ping, request, options: options);
+  }
+}
+
+abstract class UIServiceBase extends $grpc.Service {
+  $core.String get $name => 'agentapi.UI';
+
+  UIServiceBase() {
+    $addMethod($grpc.ServiceMethod<$0.Empty, $0.Empty>(
+        'Ping',
+        ping_Pre,
+        false,
+        false,
+        ($core.List<$core.int> value) => $0.Empty.fromBuffer(value),
+        ($0.Empty value) => value.writeToBuffer()));
+  }
+
+  $async.Future<$0.Empty> ping_Pre(
+      $grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
+    return ping(call, await request);
+  }
+
+  $async.Future<$0.Empty> ping($grpc.ServiceCall call, $0.Empty request);
+}
+
+class WSLInstanceClient extends $grpc.Client {
+  static final _$connected = $grpc.ClientMethod<$0.DistroInfo, $0.Port>(
+      '/agentapi.WSLInstance/Connected',
+      ($0.DistroInfo value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $0.Port.fromBuffer(value));
+
+  WSLInstanceClient($grpc.ClientChannel channel,
+      {$grpc.CallOptions? options,
+      $core.Iterable<$grpc.ClientInterceptor>? interceptors})
+      : super(channel, options: options, interceptors: interceptors);
+
+  $grpc.ResponseStream<$0.Port> connected($async.Stream<$0.DistroInfo> request,
+      {$grpc.CallOptions? options}) {
+    return $createStreamingCall(_$connected, request, options: options);
+  }
+}
+
+abstract class WSLInstanceServiceBase extends $grpc.Service {
+  $core.String get $name => 'agentapi.WSLInstance';
+
+  WSLInstanceServiceBase() {
+    $addMethod($grpc.ServiceMethod<$0.DistroInfo, $0.Port>(
+        'Connected',
+        connected,
+        true,
+        true,
+        ($core.List<$core.int> value) => $0.DistroInfo.fromBuffer(value),
+        ($0.Port value) => value.writeToBuffer()));
+  }
+
+  $async.Stream<$0.Port> connected(
+      $grpc.ServiceCall call, $async.Stream<$0.DistroInfo> request);
+}
---
+++ agentapi/dart/lib/src/agentapi.pbjson.dart
@@ -0,0 +1,41 @@
+///
+//  Generated code. Do not modify.
+//  source: agentapi.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,deprecated_member_use_from_same_package,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use distroInfoDescriptor instead')
+const DistroInfo$json = const {
+  '1': 'DistroInfo',
+  '2': const [
+    const {'1': 'wsl_name', '3': 1, '4': 1, '5': 9, '10': 'wslName'},
+    const {'1': 'id', '3': 2, '4': 1, '5': 9, '10': 'id'},
+    const {'1': 'version_id', '3': 3, '4': 1, '5': 9, '10': 'versionId'},
+    const {'1': 'pretty_name', '3': 4, '4': 1, '5': 9, '10': 'prettyName'},
+    const {'1': 'pro_attached', '3': 5, '4': 1, '5': 8, '10': 'proAttached'},
+  ],
+};
+
+/// Descriptor for `DistroInfo`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List distroInfoDescriptor = $convert.base64Decode('CgpEaXN0cm9JbmZvEhkKCHdzbF9uYW1lGAEgASgJUgd3c2xOYW1lEg4KAmlkGAIgASgJUgJpZBIdCgp2ZXJzaW9uX2lkGAMgASgJUgl2ZXJzaW9uSWQSHwoLcHJldHR5X25hbWUYBCABKAlSCnByZXR0eU5hbWUSIQoMcHJvX2F0dGFjaGVkGAUgASgIUgtwcm9BdHRhY2hlZA==');
+@$core.Deprecated('Use portDescriptor instead')
+const Port$json = const {
+  '1': 'Port',
+  '2': const [
+    const {'1': 'port', '3': 1, '4': 1, '5': 13, '10': 'port'},
+  ],
+};
+
+/// Descriptor for `Port`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List portDescriptor = $convert.base64Decode('CgRQb3J0EhIKBHBvcnQYASABKA1SBHBvcnQ=');
+@$core.Deprecated('Use emptyDescriptor instead')
+const Empty$json = const {
+  '1': 'Empty',
+};
+
+/// Descriptor for `Empty`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List emptyDescriptor = $convert.base64Decode('CgVFbXB0eQ==');
