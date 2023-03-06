382ba7a249ef58d1509b45c0b6b9d43489bfeb65
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-03-06 09:21:25 +0100

    Added comment to worker/export_test.go

--- windows-agent/internal/distros/worker/export_test.go
+++ windows-agent/internal/distros/worker/export_test.go
@@ -3,6 +3,8 @@ package worker
 // TaskQueueSize is the number of tasks that can be enqueued.
 const TaskQueueSize = taskQueueSize
 
+// QueueLen returns the number of tasks queued up. Any task currently being
+// processed is not counted.
 func (w *Worker) QueueLen() int {
 	w.manager.mu.Lock()
 	defer w.manager.mu.Unlock()
