419d560be78333308f7c13bc3bdb9891d69b7555
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-03-08 09:52:12 -0300

    Fix analysis warnings on Dart placeholder code.
    The fix contents are not relevant because nothing is really working at this point.
    The aim is to set and demonstrate the CI

--- gui/packages/p4w_ms_store/test/p4w_ms_store_test.dart
+++ gui/packages/p4w_ms_store/test/p4w_ms_store_test.dart
@@ -1,28 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:p4w_ms_store/p4w_ms_store.dart';
-import 'package:p4w_ms_store/p4w_ms_store_platform_interface.dart';
 import 'package:p4w_ms_store/p4w_ms_store_method_channel.dart';
+import 'package:p4w_ms_store/p4w_ms_store_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockP4wMsStorePlatform
-    with MockPlatformInterfaceMixin
-    implements P4wMsStorePlatform {
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
-
 void main() {
-  final P4wMsStorePlatform initialPlatform = P4wMsStorePlatform.instance;
+  final initialPlatform = P4wMsStorePlatform.instance;
 
   test('$MethodChannelP4wMsStore is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelP4wMsStore>());
   });
 
   test('getPlatformVersion', () async {
-    P4wMsStore p4wMsStorePlugin = P4wMsStore();
-    MockP4wMsStorePlatform fakePlatform = MockP4wMsStorePlatform();
+    final p4wMsStorePlugin = P4wMsStore();
+    final fakePlatform = MockP4wMsStorePlatform();
     P4wMsStorePlatform.instance = fakePlatform;
 
     expect(await p4wMsStorePlugin.getPlatformVersion(), '42');
   });
+}
+
+class MockP4wMsStorePlatform
+    with MockPlatformInterfaceMixin
+    implements P4wMsStorePlatform {
+  @override
+  Future<String?> getPlatformVersion() => Future.value('42');
 }
--- gui/packages/ubuntupro/linux/flutter/generated_plugin_registrant.cc
+++ gui/packages/ubuntupro/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <p4w_ms_store/p4w_ms_store_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) p4w_ms_store_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "P4wMsStorePlugin");
+  p4w_ms_store_plugin_register_with_registrar(p4w_ms_store_registrar);
 }
