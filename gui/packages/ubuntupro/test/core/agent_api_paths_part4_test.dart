5035b23648797376e920346d0aa2f57de8ec8ada
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-03-15 15:36:33 -0300

    Implements and applies an Environment singleton
    So we can override the env variables map for testing.
    
    It's customizability is meant only for testing.
    If we need something similar in production
    then the singleton will be a hindrance
    thus we should drop this in favor of a more robust solution
    like dotenv: https://pub.dev/packages/dotenv

---
+++ gui/packages/ubuntupro/test/core/agent_api_paths_part4_test.dart
@@ -0,0 +1,20 @@
+@TestOn('windows')
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubuntupro/core/agent_api_paths.dart';
+import 'package:ubuntupro/core/environment.dart';
+
+void main() {
+  // Because this is a singleton (a static lasting as long as the application)
+  // we need to apply the overrides early in main and they will last until
+  // main exits.
+  final _ = Environment(overrides: {'LOCALAPPDATA': null, 'USERPROFILE': null});
+
+  test('complete failure due environment', () {
+    const appName = 'AwesomeApp';
+
+    final dir = agentAddrFilePath(appName, 'addr');
+
+    expect(dir, isNull);
+  });
+}
