74591ca8bbd97f21ca786e9275c383cb9aa8a92e
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-03-15 11:05:02 -0300

    Groups some constant defaults
    Values that will be referred over and over through the app.

---
+++ gui/packages/ubuntupro/lib/constants.dart
@@ -0,0 +1,11 @@
+/// The address where the Agent gRPC service is hosted.
+const kDefaultHost = '127.0.0.1';
+
+/// This app's name. Needed to find the Agent's addr file.
+const kAppName = 'Ubuntu Pro';
+
+/// The name of the file where the Agent's drop its service connection information.
+const kAddrFileName = 'addr';
+
+/// The default border marging.
+const kDefaultMarging = 32.0;
