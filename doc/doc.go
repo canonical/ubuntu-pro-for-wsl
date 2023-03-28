2dd78551d132f436d379c9281c2650e0cbc9d894
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-03-28 11:22:30 +0200

    Created doc directory

---
+++ doc/doc.go
@@ -0,0 +1,25 @@
+// Package doc embeds structured UP4W documentation.
+package doc
+
+import (
+	"embed"
+	"fmt"
+	"path/filepath"
+	"reflect"
+)
+
+// Dir is the embedded directory containing documentation
+// Only embed structured documentation.
+//
+//go:embed *-*.md
+var Dir embed.FS
+
+// SplitFilesToken is the prefix between multiple documentation pages.
+const SplitFilesToken = "----==----"
+
+type foo struct{}
+
+// GetPackageURL returns a distant link to a raw binary content related to this directory.
+func GetPackageURL() string {
+	return fmt.Sprintf("https://%s/raw/main/doc", filepath.Dir(reflect.TypeOf(foo{}).PkgPath()))
+}
