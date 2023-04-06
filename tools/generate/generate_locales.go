eeeb41f3a180d71d1a1553fc419a7e747d80e7ae
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-04-06 15:20:03 +0200

    Created generate config for windows-agent and wsl-pro-service

---
+++ tools/generate/generate_locales.go
@@ -0,0 +1,11 @@
+// Package main generates pot, po, and mo files to enable i18n.
+// Use `go run generate_locales.go help` to see usage.
+package main
+
+import (
+	"github.com/canonical/ubuntu-pro-for-windows/tools/generate/internal/locales"
+)
+
+func main() {
+	locales.Main()
+}
---
+++ windows-agent/generate/generate-locales.go
@@ -0,0 +1,7 @@
+//go:build: tools
+
+// Package generate generates the i18n files
+package generate
+
+//go:generate go run ../../tools/generate/generate_locales.go update-po generate.yaml
+//go:generate go run ../../tools/generate/generate_locales.go generate-mo generate.yaml
