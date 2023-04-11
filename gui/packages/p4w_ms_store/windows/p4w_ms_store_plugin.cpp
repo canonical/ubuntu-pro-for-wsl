fe3867f2a9a483409222160d636b396a1109599c
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-04-10 22:54:16 -0300

    Updates the plugin class
    With those changes it must stay stable.
    Reduces the chance of breaking connection with the Flutter framework.
    Most of this file will be and remain framework boilerplate only.

--- gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
+++ gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
@@ -1,16 +1,15 @@
 #include "p4w_ms_store_plugin.h"
 
-// This must be included before many other Windows headers.
-#include <windows.h>
-
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
+#include <windows.h>
 
 #include <memory>
 #include <sstream>
+
+#include "p4w_channel_constants.h"
+#include "p4w_methods.h"
 
 namespace p4w_ms_store {
 
@@ -19,7 +18,7 @@ void P4wMsStorePlugin::RegisterWithRegistrar(
     flutter::PluginRegistrarWindows* registrar) {
   auto channel =
       std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "p4w_ms_store",
+          registrar->messenger(), Constants::ChannelName,
           &flutter::StandardMethodCodec::GetInstance());
 
   auto plugin = std::make_unique<P4wMsStorePlugin>();
@@ -39,19 +38,12 @@ P4wMsStorePlugin::~P4wMsStorePlugin() {}
 void P4wMsStorePlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue>& method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
+  try {
+    Method m(method_call);
+    m.call(std::move(result));
+    return;
+  } catch (const std::invalid_argument& err) {
+    result->Error(Constants::ChannelName, err.what());
   }
 }
 
