4dc4cb15f752b452f8419ce30821f2e0d3e5ca85
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-05-24 11:33:47 -0300

    Implements the "purchaseSubscription" method call
    By
    
    - connecting the pieces together in the plugin cpp
    - handling the arguments sent from the Dart layer
    - setting the sources in CMake
    - setting a requirement for C++20 std.

--- gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
+++ gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
@@ -37,8 +37,18 @@ P4wMsStorePlugin::~P4wMsStorePlugin() {}
 void P4wMsStorePlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue>& method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("") == 0) {
-    
+  if (method_call.method_name().compare("purchaseSubscription") == 0) {
+    // std::get_if can handle a nullptr argument.
+    auto* product = std::get_if<std::string>(method_call.arguments());
+    if (product == nullptr) {
+      result->Error(channelName, "A <productId> string argument was expected");
+    }
+
+    // When HandleMethodCall gets called we already have a running app, thus a
+    // top level window.
+    HWND topLevel = getRootWindow();
+    PurchaseSubscription(topLevel, *product, std::move(result));
+    return;
   } else {
     result->NotImplemented();
   }
