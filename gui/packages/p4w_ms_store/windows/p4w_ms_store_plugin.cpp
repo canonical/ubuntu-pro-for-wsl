6da95c04b5eacd9876603a157435a21b840b4d75
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-05-24 10:50:34 -0300

    Removes unnecessary getPlatformVersion method
    That's just demo code.
    This place will receive the "purchaseSubscription" implementation.

--- gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
+++ gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin.cpp
@@ -1,16 +1,10 @@
 #include "p4w_ms_store_plugin.h"
 
-// This must be included before many other Windows headers.
-#include <windows.h>
-
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <VersionHelpers.h>
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
 #include <memory>
-#include <sstream>
 
 namespace p4w_ms_store {
 
@@ -39,17 +33,8 @@ P4wMsStorePlugin::~P4wMsStorePlugin() {}
 void P4wMsStorePlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue>& method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
-    }
-    result->Success(flutter::EncodableValue(version_stream.str()));
+  if (method_call.method_name().compare("") == 0) {
+    
   } else {
     result->NotImplemented();
   }
