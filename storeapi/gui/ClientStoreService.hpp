86dfc2750b7390845cd76724357ce37205b88256
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-05-31 09:10:47 -0300

    Warns about keeping the enum in sync with Dart
    in the C++ code, mirroring the comment in the Dart side of the plugin

--- storeapi/gui/ClientStoreService.hpp
+++ storeapi/gui/ClientStoreService.hpp
@@ -9,6 +9,9 @@ namespace StoreApi {
 // different languages involved. Since we don't control the Windows Runtime
 // APIs, it wouldn't be future-proof to return the raw value of
 // StorePurchaseStatus enum right away.
+// This must strictly in sync the Dart PurchaseStatus enum in
+// https://github.com/canonical/ubuntu-pro-for-windows/blob/main/gui/packages/p4w_ms_store/lib/p4w_ms_store_platform_interface.dart#L8-L16
+// so we don't misinterpret the native call return values.
 enum class PurchaseStatus : std::int8_t {
   Succeeded = 0,
   AlreadyPurchased = 1,
