b863024d290d252d59e5efbe4e07d01e41f512a9
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-13 11:15:06 -0300

    Moves the constants to client.go
    They are few enough to be kept in the same source file where used.

--- windows-agent/internal/contracts/client.go
+++ windows-agent/internal/contracts/client.go
@@ -32,6 +32,24 @@ func NewClient(base *url.URL, doer httpDoer) *Client {
 	}
 }
 
+const (
+	apiVersion = "/v1"
+
+	// endpoints.
+	tokenPath        = "/token"
+	subscriptionPath = "/subscription"
+
+	// A safe token response size - tests with the real MS APIs suggested that those tokens will stay in between 1.2kB to 1.7kB.
+	// Our Pro Token is much, much smaller.
+	apiTokenMaxSize = 4096
+
+	// JSON keys commonly referred in the Contracts Server backend REST API.
+	//nolint:gosec // G101 false positive, this is not a credential
+	adTokenKey  = "azure_ad_token"
+	jwtKey      = "ms_store_id_key"
+	proTokenKey = "contract_token"
+)
+
 // GetServerAccessToken returns a short-lived auth token identifying the Contract Server backend.
 func (c *Client) GetServerAccessToken(ctx context.Context) (t string, err error) {
 	defer decorate.OnError(&err, "GetServerAccessToken")
