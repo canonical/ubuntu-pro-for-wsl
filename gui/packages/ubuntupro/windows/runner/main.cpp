988196c02f22ca7a43b0f2cbef91070a0fbf0658
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-15 11:44:30 -0300

    Default window size 800x500
    This allows the intended SubscribeNowPage contents
    to not depend on scrolling.

--- gui/packages/ubuntupro/windows/runner/main.cpp
+++ gui/packages/ubuntupro/windows/runner/main.cpp
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(800, 450); //16:9
+  Win32Window::Size size(800, 500);
   if (!window.Create(L"ubuntupro", origin, size)) {
     return EXIT_FAILURE;
   }
