1ca8f605ec3c9f41ee6504433e03dc8b165c94da
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-16 09:53:46 -0300

    Regenerates gRPC code

--- agentapi/dart/lib/src/agentapi.pbgrpc.dart
+++ agentapi/dart/lib/src/agentapi.pbgrpc.dart
@@ -1,18 +1,25 @@
-///
+//
 //  Generated code. Do not modify.
 //  source: agentapi.proto
 //
 // @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
 
 import 'dart:async' as $async;
-
 import 'dart:core' as $core;
 
 import 'package:grpc/service_api.dart' as $grpc;
+import 'package:protobuf/protobuf.dart' as $pb;
+
 import 'agentapi.pb.dart' as $0;
+
 export 'agentapi.pb.dart';
 
+@$pb.GrpcServiceName('agentapi.UI')
 class UIClient extends $grpc.Client {
   static final _$applyProToken = $grpc.ClientMethod<$0.ProAttachInfo, $0.Empty>(
       '/agentapi.UI/ApplyProToken',
@@ -26,19 +33,19 @@ class UIClient extends $grpc.Client {
   UIClient($grpc.ClientChannel channel,
       {$grpc.CallOptions? options,
       $core.Iterable<$grpc.ClientInterceptor>? interceptors})
-      : super(channel, options: options, interceptors: interceptors);
+      : super(channel, options: options,
+        interceptors: interceptors);
 
-  $grpc.ResponseFuture<$0.Empty> applyProToken($0.ProAttachInfo request,
-      {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$0.Empty> applyProToken($0.ProAttachInfo request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$applyProToken, request, options: options);
   }
 
-  $grpc.ResponseFuture<$0.Empty> ping($0.Empty request,
-      {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$0.Empty> ping($0.Empty request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$ping, request, options: options);
   }
 }
 
+@$pb.GrpcServiceName('agentapi.UI')
 abstract class UIServiceBase extends $grpc.Service {
   $core.String get $name => 'agentapi.UI';
 
@@ -59,21 +66,18 @@ abstract class UIServiceBase extends $grpc.Service {
         ($0.Empty value) => value.writeToBuffer()));
   }
 
-  $async.Future<$0.Empty> applyProToken_Pre(
-      $grpc.ServiceCall call, $async.Future<$0.ProAttachInfo> request) async {
+  $async.Future<$0.Empty> applyProToken_Pre($grpc.ServiceCall call, $async.Future<$0.ProAttachInfo> request) async {
     return applyProToken(call, await request);
   }
 
-  $async.Future<$0.Empty> ping_Pre(
-      $grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
+  $async.Future<$0.Empty> ping_Pre($grpc.ServiceCall call, $async.Future<$0.Empty> request) async {
     return ping(call, await request);
   }
 
-  $async.Future<$0.Empty> applyProToken(
-      $grpc.ServiceCall call, $0.ProAttachInfo request);
+  $async.Future<$0.Empty> applyProToken($grpc.ServiceCall call, $0.ProAttachInfo request);
   $async.Future<$0.Empty> ping($grpc.ServiceCall call, $0.Empty request);
 }
-
+@$pb.GrpcServiceName('agentapi.WSLInstance')
 class WSLInstanceClient extends $grpc.Client {
   static final _$connected = $grpc.ClientMethod<$0.DistroInfo, $0.Port>(
       '/agentapi.WSLInstance/Connected',
@@ -83,14 +87,15 @@ class WSLInstanceClient extends $grpc.Client {
   WSLInstanceClient($grpc.ClientChannel channel,
       {$grpc.CallOptions? options,
       $core.Iterable<$grpc.ClientInterceptor>? interceptors})
-      : super(channel, options: options, interceptors: interceptors);
+      : super(channel, options: options,
+        interceptors: interceptors);
 
-  $grpc.ResponseStream<$0.Port> connected($async.Stream<$0.DistroInfo> request,
-      {$grpc.CallOptions? options}) {
+  $grpc.ResponseStream<$0.Port> connected($async.Stream<$0.DistroInfo> request, {$grpc.CallOptions? options}) {
     return $createStreamingCall(_$connected, request, options: options);
   }
 }
 
+@$pb.GrpcServiceName('agentapi.WSLInstance')
 abstract class WSLInstanceServiceBase extends $grpc.Service {
   $core.String get $name => 'agentapi.WSLInstance';
 
@@ -104,6 +109,5 @@ abstract class WSLInstanceServiceBase extends $grpc.Service {
         ($0.Port value) => value.writeToBuffer()));
   }
 
-  $async.Stream<$0.Port> connected(
-      $grpc.ServiceCall call, $async.Stream<$0.DistroInfo> request);
+  $async.Stream<$0.Port> connected($grpc.ServiceCall call, $async.Stream<$0.DistroInfo> request);
 }
