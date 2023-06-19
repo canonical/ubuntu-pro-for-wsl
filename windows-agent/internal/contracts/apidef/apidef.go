2f0e739809ccaba3a333d2bfd7f996c3db951023
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-19 10:02:04 -0300

    Doc comments on apidef.go
    The existing comments were not suitable for exported symbols.

--- windows-agent/internal/contracts/apidef/apidef.go
+++ windows-agent/internal/contracts/apidef/apidef.go
@@ -2,19 +2,23 @@
 package apidef
 
 const (
+	// Version is the current Contracts Server REST API version.
 	Version = "/v1"
 
-	// endpoints.
-	TokenPath        = "/token"
+	//TokenPath is the path where clients should GET the Azure AD ephemeral token.
+	TokenPath = "/token"
+	// SubscriptionPath is the path where clients should POST the user JWT to notify the CS backend of changes in the current user subscription.
 	SubscriptionPath = "/subscription"
 
-	// A safe token response size - tests with the real MS APIs suggested that those tokens will stay in between 1.2kB to 1.7kB.
+	// TokenMaxSize is a safe token response size - tests with the real MS APIs suggested that those tokens will stay in between 1.2kB to 1.7kB.
 	// Our Pro Token is much, much smaller.
 	TokenMaxSize = 4096
 
-	// JSON keys commonly referred in the Contracts Server backend REST API.
 	//nolint:gosec // G101 false positive, this is not a credential
-	ADTokenKey  = "azure_ad_token"
-	JWTKey      = "ms_store_id_key"
+	// ADTokenKey is the JSON key of the response payload of the /token endpoint.
+	ADTokenKey = "azure_ad_token"
+	// JWTKey is the JSON key of the request payload of the /susbcription endpoint.
+	JWTKey = "ms_store_id_key"
+	// ProTokenKey is the JSON key of a successful response payload of the /subscription endpoint.
 	ProTokenKey = "contract_token"
 )
