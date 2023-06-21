e2cf54a6ab604833a9704e42a89339b8b62ab5cd
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-21 17:33:32 -0300

    No pointer size assumptions in the ABI boundary
    Go will call us with uintptr's, which are unsigned and large enough to hold any pointer.
    The equivalent for that is C99 uintptr_t (also C++11 and forward).
    On pointers we are safe, but accepting uintptrs into a int32_t is a narrowing conversion.
    
    To be more generic and future proof we typedef from intptr_t, preserving the integer value signal on non-pointer values.

--- msix/storeapi/StoreApi.cpp
+++ msix/storeapi/StoreApi.cpp
@@ -2,32 +2,32 @@
 
 #include "framework.hpp"
 
-// Syntactic sugar to convert the enum [value] into a int64_t.
-constexpr int64_t toInt64(Errors value) { return static_cast<int64_t>(value); }
+// Syntactic sugar to convert the enum [value] into a Int.
+constexpr Int toInt(Errors value) { return static_cast<Int>(value); }
 
 // The maximum token length expected.
-static constexpr int32_t MaxTokenLen = 4096;
+static constexpr Int MaxTokenLen = 4096;
 
 // The maximum product ID string length expected as an input. In practice the
 // size is much lower. This reserves room for the future.
-static constexpr int32_t MaxProductIdLen = 128;
+static constexpr Int MaxProductIdLen = 128;
 
 // Groups together a char pointer and a length to prevent easy swappable
 // parameters in function calls.
 struct RawString {
   const char* data;
-  int32_t length;
+  Int length;
 };
 
 // Sanity checks the [input] string argument against some non-sensical mistakes
 // (such as negative length) and against a [maxLength] allowed by the caller.
-Errors validateArg(RawString input, int32_t maxLength);
+Errors validateArg(RawString input, Int maxLength);
 
-int64_t GetSubscriptionExpirationDate(const char* productID, int32_t length) {
+Int GetSubscriptionExpirationDate(const char* productID, Int length) {
   if (auto err =
           validateArg({.data = productID, .length = length}, MaxProductIdLen);
       err != Errors::None) {
-    return toInt64(err);
+    return toInt(err);
   }
 
   try {
@@ -35,30 +35,30 @@ int64_t GetSubscriptionExpirationDate(const char* productID, int32_t length) {
 
     const std::time_t expiration =
         service
-            .CurrentExpirationDate({productID, static_cast<uint64_t>(length)})
+            .CurrentExpirationDate({productID, static_cast<std::size_t>(length)})
             .get();
 
     return expiration;
   } catch (const StoreApi::Exception&) {
-    return toInt64(Errors::StoreAPI);
+    return toInt(Errors::StoreAPI);
   } catch (const winrt::hresult_error&) {
-    return toInt64(Errors::WinRT);
+    return toInt(Errors::WinRT);
   } catch (const std::exception&) {
-    return toInt64(Errors::Unknown);
+    return toInt(Errors::Unknown);
   }
 }
 
-int64_t GenerateUserJWT(const char* accessToken, int32_t accessTokenLen,
+Int GenerateUserJWT(const char* accessToken, Int accessTokenLen,
                         char** jwtBuf) {
   if (auto err = validateArg({.data = accessToken, .length = accessTokenLen},
                              MaxTokenLen);
       err != Errors::None) {
-    return toInt64(err);
+    return toInt(err);
   }
 
   try {
     auto user = StoreApi::UserInfo::Current().get();
-    std::string serverToken{accessToken, static_cast<uint64_t>(accessTokenLen)};
+    std::string serverToken{accessToken, static_cast<std::size_t>(accessTokenLen)};
 
     StoreApi::ServerStoreService service{};
     const std::string jwt = service.GenerateUserJwt(serverToken, user).get();
@@ -66,10 +66,10 @@ int64_t GenerateUserJWT(const char* accessToken, int32_t accessTokenLen,
     // Allocates memory using some OS API so we can free this buffer on the
     // other side of the ABI without assumptions on specifics of the programming
     // language runtime in their side.
-    const int64_t length = jwt.size();
+    const Int length = jwt.size();
     auto* buffer = static_cast<char*>(::CoTaskMemAlloc(length));
     if (buffer == nullptr) {
-      return toInt64(Errors::AllocationFailure);
+      return toInt(Errors::AllocationFailure);
     }
 
     std::memcpy(buffer, jwt.c_str(), length);
@@ -77,15 +77,15 @@ int64_t GenerateUserJWT(const char* accessToken, int32_t accessTokenLen,
     return length;
 
   } catch (const StoreApi::Exception&) {
-    return toInt64(Errors::StoreAPI);
+    return toInt(Errors::StoreAPI);
   } catch (const winrt::hresult_error&) {
-    return toInt64(Errors::WinRT);
+    return toInt(Errors::WinRT);
   } catch (const std::exception&) {
-    return toInt64(Errors::Unknown);
+    return toInt(Errors::Unknown);
   }
 }
 
-Errors validateArg(RawString input, int32_t maxLength) {
+Errors validateArg(RawString input, Int maxLength) {
   if (input.data == nullptr) {
     return Errors::Null;
   }
--- msix/storeapi/StoreApi.hpp
+++ msix/storeapi/StoreApi.hpp
@@ -1,33 +1,40 @@
-// Exports two functions exposing information about a product subscription from
+// Exports functions exposing information about a product subscription from
 // MS Store that the background agent is interested in. They both follow a
-// simple protocol of returning negative int64_t values for errors that the
+// simple protocol of returning negative integer values for errors that the
 // caller can translate to the most suitable error system used in their side of
 // the ABI. Positive values are the actual response, which may or may not
 // combine with further output parameters. Zero should never be returned.
 #pragma once
 
 #include <agent/ServerStoreService.hpp>
+#include <cstdint>
+
+extern "C" {
+// Go will call us with uintptr's, which are unsigned and large enough to hold
+// any pointer. The equivalent for that is C99 uintptr_t (also C++11 and
+// forward). On pointers we are safe, but accepting uintptrs into a int32_t is a
+// narrowing conversion on x64 platforms. On those platforms we could rely on
+// uintptr_t being the same as uint64_t. To be more generic and future proof we
+// typedef from intptr_t (the signed version that can still hold any pointer),
+// so we can preserve the signed nature of actual integer (non-pointer) values.
+using Int = std::intptr_t;
 
 #define DLL_EXPORT __declspec(dllexport)
 
-extern "C" {
 // Returns a positive integer representing the UNIX time of the current user
 // subscription expiration date.
-DLL_EXPORT int64_t GetSubscriptionExpirationDate(const char* productID,
-                                                 int32_t length);
+DLL_EXPORT Int GetSubscriptionExpirationDate(const char* productID, Int length);
 
 // Outputs the user JWT via the [jwtBuf] output parameter and returns the buffer
 // length. The caller is responsible for freeing the memory region pointed by
 // [jwtBuf] by calling CoTaskMemFree.
-DLL_EXPORT int64_t GenerateUserJWT(const char* accessToken,
-                                   int32_t accessTokenLen,
-                                   // output
-                                   char** jwtBuf);
-}
+DLL_EXPORT Int GenerateUserJWT(const char* accessToken, Int accessTokenLen,
+                               // output
+                               char** jwtBuf);
 
 // Document error constants so we can translate those as Go errors.
-enum class Errors : int64_t {
-  NotSubscribed = std::numeric_limits<int64_t>::lowest(),
+enum class Errors : Int {
+  NotSubscribed = std::numeric_limits<Int>::lowest(),
   AllocationFailure = -10,
   // input string argument errors
   Null = -9,
@@ -41,3 +48,4 @@ enum class Errors : int64_t {
   // Not an error.
   None = 0,
 };
+}
