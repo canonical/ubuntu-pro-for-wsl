7191c72c7b0993033d7e109c222e18d263863f3d
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-23 15:20:10 -0300

    Implements the actual page.

---
+++ gui/packages/ubuntupro/lib/pages/subscription_status/susbcription_status_page.dart
@@ -0,0 +1,60 @@
+import 'package:agentapi/agentapi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
+
+import '../../core/agent_api_client.dart';
+import 'subscription_status_model.dart';
+import 'subscription_status_widgets.dart';
+
+/// The page to be shown when we have an active Pro subscription. The exact contents will match the type of the subscription
+/// (i.e., whether the Pro token was set manually, through MS Store or provided by the user's Organization)
+class SubscriptionStatusPage extends StatelessWidget {
+  const SubscriptionStatusPage({super.key});
+
+  // TODO: Replace the constants below with YaruColors.dark.link and YaruColors.dark.error once we release yaru v0.9 or v1.0.0.
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<SubscriptionStatusModel>();
+    final lang = AppLocalizations.of(context);
+
+    return switch (model) {
+      StoreSubscriptionStatusModel() => SubscriptionStatus(
+          caption: lang.storeManaged,
+          actionButton: TextButton(
+            onPressed: model.launchManagementWebPage,
+            style: TextButton.styleFrom(
+              foregroundColor: const Color(0xFF0094FF),
+            ),
+            child: Text(lang.manageSubscription),
+          ),
+        ),
+      ManualSubscriptionStatusModel() => SubscriptionStatus(
+          caption: lang.manuallyManaged,
+          actionButton: FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: const Color(0xFFE86581),
+            ),
+            onPressed: model.detachPro,
+            child: Text(lang.detachPro),
+          ),
+        ),
+      OrgSubscriptionStatusModel() => SubscriptionStatus(
+          caption: lang.orgManaged,
+        ),
+    };
+  }
+
+  /// Initializes the view-model and inject it in the widget tree so the child page can access it via the BuildContext.
+  static Widget create(BuildContext context) {
+    return Provider(
+      create: (context) {
+        final client = getService<AgentApiClient>();
+        final subscriptionInfo = context.read<SubscriptionInfo>();
+        return SubscriptionStatusModel(subscriptionInfo, client);
+      },
+      child: const SubscriptionStatusPage(),
+    );
+  }
+}
---
+++ gui/packages/ubuntupro/test/pages/subscription_status/subscription_status_page_test.dart
@@ -0,0 +1,88 @@
+import 'package:agentapi/agentapi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
+import 'package:ubuntupro/core/agent_api_client.dart';
+import 'package:ubuntupro/pages/subscription_status/subscription_status_model.dart';
+import 'package:ubuntupro/pages/subscription_status/susbcription_status_page.dart';
+
+void main() {
+  group('subscription info', () {
+    final client = FakeAgentApiClient();
+    final info = SubscriptionInfo();
+    testWidgets('manual', (tester) async {
+      info.ensureManual();
+      final app = buildApp(info, client);
+
+      await tester.pumpWidget(app);
+
+      final context = tester.element(find.byType(SubscriptionStatusPage));
+      final lang = AppLocalizations.of(context);
+
+      expect(find.text(lang.detachPro), findsOneWidget);
+    });
+
+    testWidgets('store', (tester) async {
+      info.ensureMicrosoftStore();
+      final app = buildApp(info, client);
+
+      await tester.pumpWidget(app);
+
+      final context = tester.element(find.byType(SubscriptionStatusPage));
+      final lang = AppLocalizations.of(context);
+
+      expect(find.text(lang.manageSubscription), findsOneWidget);
+    });
+
+    testWidgets('organization', (tester) async {
+      info.ensureOrganization();
+      final app = buildApp(info, client);
+
+      await tester.pumpWidget(app);
+
+      final context = tester.element(find.byType(SubscriptionStatusPage));
+      final lang = AppLocalizations.of(context);
+
+      expect(find.text(lang.orgManaged), findsOneWidget);
+    });
+  });
+  testWidgets('creates a model', (tester) async {
+    final mockClient = FakeAgentApiClient();
+    final info = SubscriptionInfo();
+    info.ensureManual();
+    registerServiceInstance<AgentApiClient>(mockClient);
+    final app = Provider.value(
+      value: info,
+      child: const MaterialApp(
+        routes: {'/': SubscriptionStatusPage.create},
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+      ),
+    );
+
+    await tester.pumpWidget(app);
+    await tester.pumpAndSettle();
+
+    final context = tester.element(find.byType(SubscriptionStatusPage));
+    final model = Provider.of<SubscriptionStatusModel>(context, listen: false);
+
+    expect(model, isNotNull);
+  });
+}
+
+Widget buildApp(SubscriptionInfo info, AgentApiClient client) {
+  final model = SubscriptionStatusModel(info, client);
+  return MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    home: Provider.value(
+      value: model,
+      child: const SubscriptionStatusPage(),
+    ),
+  );
+}
+
+class FakeAgentApiClient implements AgentApiClient {
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
