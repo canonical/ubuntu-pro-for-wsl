9666bf6073b39c3b8441df1b1567d0a6beda5a3a
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-06-23 12:09:17 -0300

    Implements the page contents widget
    Defining the common page structure
    The exact contents will be decided in the upper level.

---
+++ gui/packages/ubuntupro/lib/pages/subscription_status/subscription_status_widgets.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:yaru/yaru.dart';
+import '../widgets/page_widgets.dart';
+
+/// A page content widget built on top of the Dark styled landing page showing the current user active subscription
+/// feedback and an optional action button in a column layout.
+class SubscriptionStatus extends StatelessWidget {
+  const SubscriptionStatus({
+    super.key,
+    required this.caption,
+    this.actionButton,
+  });
+
+  /// The caption to render below the active subscription subtitle.
+  final String caption;
+
+  /// The optional action button matching the capabilities of the current subscription type.
+  final Widget? actionButton;
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = AppLocalizations.of(context);
+
+    return DarkStyledLandingPage(
+      children: [
+        Row(
+          children: [
+            Icon(
+              Icons.check_circle,
+              color: yaruDark.textTheme.bodyLarge!.color,
+            ),
+            const SizedBox(width: 8.0),
+            Text(
+              lang.subscriptionIsActive,
+              style: yaruDark.textTheme.bodyLarge!
+                  .copyWith(fontWeight: FontWeight.w100),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          caption,
+          style: yaruDark.textTheme.bodyMedium!.copyWith(
+            fontWeight: FontWeight.w100,
+            color: YaruColors.warmGrey,
+          ),
+        ),
+        if (actionButton != null)
+          Padding(
+            padding: const EdgeInsets.only(top: 32.0),
+            child: actionButton!,
+          ),
+      ],
+    );
+  }
+}
---
+++ gui/packages/ubuntupro/test/pages/subscription_status/susbcription_status_widgets_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ubuntupro/pages/subscription_status/subscription_status_widgets.dart';
+
+void main() {
+  group('subscription status', () {
+    const caption = 'my caption';
+    const buttonName = 'my button';
+
+    testWidgets('caption', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: SubscriptionStatus(caption: caption),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+        ),
+      );
+
+      expect(find.text(caption), findsOneWidget);
+    });
+
+    testWidgets('action button', (tester) async {
+      var clicked = false;
+      await tester.pumpWidget(
+        MaterialApp(
+          home: SubscriptionStatus(
+            caption: caption,
+            actionButton: TextButton(
+              onPressed: () => clicked = true,
+              child: const Text(buttonName),
+            ),
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+        ),
+      );
+
+      final button = find.byType(TextButton);
+      expect(button, findsOneWidget);
+      expect(clicked, isFalse);
+
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+      expect(clicked, isTrue);
+    });
+  });
+}
