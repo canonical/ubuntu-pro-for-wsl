958dc94075c9bbcb1e62646b73c9225cd2f5e71c
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-07-05 11:46:56 +0200

    Implement TestInstallFromExecutable

--- windows-agent/internal/proservices/landscape/distroinstall/distroinstall_test.go
+++ windows-agent/internal/proservices/landscape/distroinstall/distroinstall_test.go
@@ -122,3 +122,58 @@ func TestCreateUser(t *testing.T) {
 		})
 	}
 }
+
+func TestInstallFromExecutable(t *testing.T) {
+	if wsl.MockAvailable() {
+		t.Parallel()
+	}
+
+	testCases := map[string]struct {
+		distroName string
+		mockErr    bool
+
+		wantErr bool
+	}{
+		"Success": {distroName: "Ubuntu-22.04"},
+
+		// We have nothing against debian, this simply fails the injection safety check without failing wsl --install
+		"Error when the distro has not a valid name": {distroName: "Debian", wantErr: true},
+		"Error when the distro registration fails":   {distroName: "Ubuntu-22.04", mockErr: true, wantErr: true},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
+			if wsl.MockAvailable() {
+				m := wslmock.New()
+				m.WslRegisterDistributionError = tc.mockErr
+				defer m.ResetErrors()
+
+				ctx = wsl.WithMock(ctx, m)
+			} else if tc.mockErr {
+				t.Skip("This test is only available with the mock enabled")
+			}
+
+			err := wsl.Install(ctx, tc.distroName)
+			require.NoError(t, err, "Setup: Install should return no errors")
+
+			d := wsl.NewDistro(ctx, tc.distroName)
+			defer d.Uninstall(ctx) //nolint:errcheck // We don't care
+
+			err = distroinstall.InstallFromExecutable(ctx, d)
+			if tc.wantErr {
+				require.Error(t, err, "InstallFromExecutable should return an error")
+				return
+			}
+			require.NoError(t, err, "InstallFromExecutable should return no errors")
+
+			r, err := d.IsRegistered()
+			require.NoError(t, err, "IsRegistered should return no errors")
+			require.True(t, r, "InstallFromExecutable should have registered the distro")
+
+			err = distroinstall.InstallFromExecutable(ctx, d)
+			require.Error(t, err, "InstallFromExecutable should return an error when the distro is already installed")
+		})
+	}
+}
