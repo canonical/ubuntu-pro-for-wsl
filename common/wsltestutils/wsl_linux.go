ddd2a1130b5546fa1feb228f9209589ca8c6ec7f
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-07-13 10:44:44 +0200

    Fix OS issues in common tests

--- common/wsltestutils/wsl_linux.go
+++ common/wsltestutils/wsl_linux.go
@@ -7,8 +7,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// PowershellImportDistro uses powershell.exe to import a distro from a specific rootfs.
+// This implementation is a stub.
+//
 //nolint:revive // The context is better after the testing.T
-func PowershellInstallDistro(t *testing.T, ctx context.Context, distroName string, rootFsPath string) (GUID string) {
+func PowershellImportDistro(t *testing.T, ctx context.Context, distroName string, rootFsPath string) (GUID string) {
 	t.Helper()
 
 	require.Fail(t, "Attempted to register a distro on Linux", "To run this test on Linux, you must use the mock GoWSL back-end")
@@ -16,6 +19,8 @@ func PowershellInstallDistro(t *testing.T, ctx context.Context, distroName strin
 }
 
 func powershellOutputf(t *testing.T, command string, args ...any) string {
+	t.Helper()
+
 	require.Fail(t, "Attempted to user powershell on Linux", "To run this test on Linux, you must use the mock GoWSL back-end")
 	return ""
 }
