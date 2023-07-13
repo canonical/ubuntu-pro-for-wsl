030ff45f042e4b189a5c6e954cd2b682abce72d4
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-07-13 09:30:41 +0200

    Linter pass in common package

--- common/consts.go
+++ common/consts.go
@@ -1,11 +1,11 @@
-// Package consts defines the constants used by the project
+// Package common defines the constants used by the project
 package common
 
 const (
 	// TEXTDOMAIN is the gettext domain for l10n.
 	TEXTDOMAIN = `ubuntu-pro`
 
-	// TODO: LocalAppDataDir is the relative path name used in user's cache dir to store process transient data.
+	// LocalAppDataDir is the relative path name used in user's cache dir to store process transient data.
 	//  ${env:LocalAppData}/{LocalAppDataDir}
 	LocalAppDataDir = "Ubuntu Pro"
 
--- common/version.go
+++ common/version.go
@@ -4,5 +4,7 @@ import (
 	_ "embed"
 )
 
+// Version is the version number for Ubuntu Pro For Windows
+//
 //go:embed version
 var Version string
