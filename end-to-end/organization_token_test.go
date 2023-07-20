3cf8470d36eba3e596554d4e11a044966670f427
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-07-20 12:29:30 +0200

    Expanded tests

--- end-to-end/organization_token_test.go
+++ end-to-end/organization_token_test.go
@@ -13,51 +13,91 @@ import (
 )
 
 func TestOrganizationProvidedToken(t *testing.T) {
-	ctx := context.Background()
-	testSetup(t)
+	type whenToken int
+	const (
+		never whenToken = iota
+		beforeDistroRegistration
+		afterDistroRegistration
+	)
+
+	testCases := map[string]struct {
+		whenToken whenToken
+
+		wantAttached bool
+	}{
+		"Success when the subscription is active before registration":   {whenToken: beforeDistroRegistration, wantAttached: true},
+		"Success when the subscription is activated after registration": {whenToken: afterDistroRegistration, wantAttached: true},
+
+		"Error when there is no active subscription": {whenToken: never, wantAttached: false},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
+
+			testSetup(t)
+
+			if tc.whenToken == beforeDistroRegistration {
+				activateOrgSubscription(t)
+				startAgent(t, ctx)
+			}
+
+			// Distro setup
+			name := registerFromTestImage(t, ctx)
+			d := wsl.NewDistro(ctx, name)
+
+			defer logWslProServiceJournal(t, ctx, d)
+
+			out, err := d.Command(ctx, "exit 0").CombinedOutput()
+			require.NoErrorf(t, err, "Setup: could not wake distro up: %v. %s", err, out)
+
+			if tc.whenToken == afterDistroRegistration {
+				err := d.Terminate()
+				require.NoError(t, err, "could not restart distro")
+
+				activateOrgSubscription(t)
+				startAgent(t, ctx)
+
+				out, err := d.Command(ctx, "exit 0").CombinedOutput()
+				require.NoErrorf(t, err, "Setup: could not wake distro up: %v. %s", err, out)
+			}
+
+			// Give the agent some time to pro-attach
+			time.Sleep(5 * time.Second)
+
+			// Validate that the distro was attached
+			out, err = d.Command(ctx, "pro status --format=json").Output()
+			require.NoErrorf(t, err, "Setup: could not call pro status: %v. %s", err, out)
+
+			var response struct {
+				Attached bool
+			}
+			err = json.Unmarshal(out, &response)
+			require.NoError(t, err, "could not parse pro status response: %s", out)
+
+			if !tc.wantAttached {
+				require.False(t, response.Attached, "distro should not have been Pro attached")
+				return
+			}
+
+			require.True(t, response.Attached, "distro should have been Pro attached")
+		})
+	}
+}
+
+func activateOrgSubscription(t *testing.T) {
+	t.Helper()
 
 	token := os.Getenv(proTokenEnv)
 	require.NotEmptyf(t, token, "Setup: environment variable %q should contain a valid pro token, but is empty", proTokenEnv)
 
-	// Agent setup
 	key, _, err := registry.CreateKey(registry.CURRENT_USER, registryPath, registry.WRITE)
 	require.NoErrorf(t, err, "Setup: could not open UbuntuPro registry key")
 	defer key.Close()
 
 	err = key.SetStringValue("ProTokenOrg", token)
 	require.NoError(t, err, "could not write token in registry")
-
-	err = key.Close()
-	require.NoError(t, err, "could not close registry key")
-
-	// Distro setup
-	name := registerFromTestImage(t, ctx)
-	d := wsl.NewDistro(ctx, name)
-
-	defer logWslProServiceJournal(t, ctx, d)
-
-	err = d.Terminate()
-	require.NoError(t, err, "could not restart distro")
-
-	// Start of test: start agent and mimic first boot
-	startAgent(t, ctx)
-
-	out, err := d.Command(ctx, "exit 0").CombinedOutput()
-	require.NoErrorf(t, err, "Setup: could not wake distro up: %v. %s", err, out)
-
-	// Give the agent some time to pro-attach
-	time.Sleep(5 * time.Second)
-
-	// Validate that the distro was attached
-	out, err = d.Command(ctx, "pro status --format=json").Output()
-	require.NoErrorf(t, err, "Setup: could not call pro status: %v. %s", err, out)
-
-	var response struct {
-		Attached bool
-	}
-	err = json.Unmarshal(out, &response)
-	require.NoError(t, err, "could not parse pro status response: %s", out)
-	require.True(t, response.Attached, "distro should have been Pro attached")
 }
 
 //nolint:revive // testing.T must precede the context
