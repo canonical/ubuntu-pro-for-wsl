c1e9490c19ca9a9099b720982ad27e3203f5cb29
EduardGomezEscandell <edu.gomez.escandell@canonical.com>
2023-08-11 10:53:02 +0200

    Fixed build-deb script when run by non-root user

--- tools/build/build-deb.sh
+++ tools/build/build-deb.sh
@@ -5,9 +5,9 @@ set -eu
 cd $(dirname $(realpath "$0"))/../../wsl-pro-service
 
 # Install dependencies
-apt update
-apt install -y devscripts
-apt -y build-dep .
+sudo apt update
+sudo apt install -y devscripts
+sudo apt -y build-dep .
 
 # Build
 DEB_BUILD_OPTIONS=nocheck debuild
