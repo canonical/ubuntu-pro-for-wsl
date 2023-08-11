ad4e1d649bb83640907bd86ccfe337d38f1e7837
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-08-11 12:53:14 +0200

    Set debian frontend as noninteractive

--- tools/build/build-deb.sh
+++ tools/build/build-deb.sh
@@ -5,9 +5,9 @@ set -eu
 cd $(dirname $(realpath "$0"))/../../wsl-pro-service
 
 # Install dependencies
-sudo apt update
-sudo apt install -y devscripts
-sudo apt -y build-dep .
+sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y devscripts
+sudo DEBIAN_FRONTEND=noninteractive apt -y build-dep .
 
 # Build
 DEB_BUILD_OPTIONS=nocheck debuild
