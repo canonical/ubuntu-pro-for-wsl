28e6e9a2c8e544b352350d01e51b4fff4551c625
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-08-16 11:25:21 -0300

    Uses msbuild to compile the agent
    This way we keep in sync with the build flags used to build the appx

--- gui/packages/ubuntupro/test/utils/build_agent.dart
+++ gui/packages/ubuntupro/test/utils/build_agent.dart
@@ -5,33 +5,50 @@ import 'package:path/path.dart' as p;
 /// Since this is a test helper, assertions are just fine.
 /// A try-catch block (on AssertionError) can still prevent process exit.
 Future<void> buildAgentExe(String destination) async {
+  const config = 'Debug';
+  const exeName = 'ubuntu-pro-agent.exe';
   final dest = Directory(destination);
   await dest.create(recursive: true);
 
-  final goWorkDir = await _findGoWorkspace();
-  assert(goWorkDir != null, 'Could not find the go workspace');
+  // <...>/msix/agent/agent.vcxproj
+  final vcxproj = await _findAgentVcxproj();
+  assert(vcxproj != null, 'Could not find the agent project');
 
-  final mainGo = p.normalize(p.join(goWorkDir!.path, _agentPackageDir));
-
-  final result = await Process.run(
-    'go',
-    ['build', '-ldflags', '-H=windowsgui', mainGo],
-    workingDirectory: dest.path,
+  await _build(
+    buildProgram: 'msbuild',
+    targetPath: vcxproj!,
+    arguments: ['/p:Configuration=$config'],
   );
 
-  assert(result.exitCode == 0, 'go build failed');
+  // <...>/msix/agent/Debug/ubuntu-pro-agent.exe
+  final expectedOutput = p.join(p.dirname(vcxproj), config, exeName);
+
+  await File(expectedOutput).rename(p.join(dest.absolute.path, exeName));
 }
 
-// The ubuntu-pro-agent's package path relative to the workspace directory.
-const _agentPackageDir = 'windows-agent/cmd/ubuntu-pro-agent/';
-// The go workspace definition file.
-const _goWorkspaceFile = 'go.work';
+Future<void> _build({
+  required String buildProgram,
+  List<String>? arguments,
+  required String targetPath,
+  String? workingDir,
+}) async {
+  final result = await Process.run(
+    buildProgram,
+    [...?arguments, targetPath],
+    workingDirectory: workingDir,
+  );
 
-Future<Directory?> _findGoWorkspace() =>
-    _findFileUpwards(startDir: Directory.current, filename: _goWorkspaceFile);
+  assert(result.exitCode == 0, '$buildProgram failed');
+}
 
-Future<Directory?> _findFileUpwards({
-  required String filename,
+Future<String?> _findAgentVcxproj() => _findFileUpwards(
+      startDir: Directory.current,
+      name: 'msix/agent/agent.vcxproj',
+    );
+
+/// Iterates upwards from [startDir] looking for a file matching the join "[startDir]/[name]".
+Future<String?> _findFileUpwards({
+  required String name,
   required Directory startDir,
 }) async {
   if (!await startDir.exists()) {
@@ -41,9 +58,9 @@ Future<Directory?> _findFileUpwards({
   startDir = startDir.absolute;
 
   while (true) {
-    final file = File(p.join(startDir.path, _goWorkspaceFile));
+    final file = File(p.join(startDir.path, name));
     if (await file.exists()) {
-      return startDir;
+      return file.path;
     }
 
     if (startDir.path == startDir.parent.path) {
