77de8a1c8d5a8463e7307b442d11e396b83f8d56
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-08-18 10:33:10 +0200

    Implement Windows Store error translation layer

---
+++ windows-agent/internal/contracts/microsoftstore/errors.go
@@ -0,0 +1,52 @@
+// Package microsoftstore is a wrapper around storeapi.dll
+package microsoftstore
+
+// StoreAPIError are the error constants in the store api.
+type StoreAPIError int
+
+// Keep up-to-date with msix\storeapi\StoreApi.hpp.
+const (
+	ErrNotSubscribed     StoreAPIError = -128
+	ErrAllocationFailure StoreAPIError = -10
+	ErrNullInputPtr      StoreAPIError = -9
+	ErrTooBigLength      StoreAPIError = -8
+	ErrZeroLength        StoreAPIError = -7
+	ErrNullOutputPtr     StoreAPIError = -6
+	ErrStoreAPI          StoreAPIError = -3
+	ErrWinRT             StoreAPIError = -2
+	ErrUnknown           StoreAPIError = -1
+	ErrSuccess           StoreAPIError = 0
+)
+
+// NewStoreAPIError creates StoreAPIError from the result of a call to the storeAPI DLL.
+func NewStoreAPIError(hresult uintptr) error {
+	if err := StoreAPIError(hresult); err < ErrSuccess {
+		return err
+	}
+	return nil
+}
+
+func (err StoreAPIError) Error() string {
+	switch err {
+	case ErrNotSubscribed:
+		return "not subscribed"
+	case ErrAllocationFailure:
+		return "allocation failure"
+	case ErrTooBigLength:
+		return "length too large"
+	case ErrZeroLength:
+		return "length cannot be zero"
+	case ErrNullOutputPtr:
+		return "output buffer cannot be null"
+	case ErrStoreAPI:
+		return "error at the store API"
+	case ErrWinRT:
+		return "error at the Windows Runtime"
+	case ErrUnknown:
+		return "unexpected error"
+	case ErrSuccess:
+		return "success"
+	default:
+		return "undefined"
+	}
+}
