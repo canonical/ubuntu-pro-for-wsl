d1b89d8159e694f9d04d504cb6467ab7ff889eb4
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-08-21 09:59:52 -0300

    Back to Pro4WindowsApp as StatelessWidget
    @d-loose proved me that the BuildContext captured in the closure can find the Provider.
    
    It's levels down the one creating the Provider.

--- gui/packages/ubuntupro/lib/app.dart
+++ gui/packages/ubuntupro/lib/app.dart
@@ -13,22 +13,14 @@ import 'pages/startup/startup_page.dart';
 import 'pages/subscription_status/subscription_status_page.dart';
 import 'routes.dart';
 
-class Pro4WindowsApp extends StatefulWidget {
+class Pro4WindowsApp extends StatelessWidget {
   const Pro4WindowsApp({super.key});
 
   @override
-  State<Pro4WindowsApp> createState() => _Pro4WindowsAppState();
-}
-
-class _Pro4WindowsAppState extends State<Pro4WindowsApp> {
-  // A notifier that will allow child widgets to rebuild once new information
-  // about the current subscription information arrives.
-  final _subscriptionInfo = ValueNotifier(SubscriptionInfo());
-  @override
   Widget build(BuildContext context) {
     return YaruTheme(
-      builder: (context, yaru, child) => ChangeNotifierProvider.value(
-        value: _subscriptionInfo,
+      builder: (context, yaru, child) => ChangeNotifierProvider(
+        create: (_) => ValueNotifier(SubscriptionInfo()),
         child: MaterialApp(
           title: kAppName,
           theme: yaru.theme,
@@ -43,7 +35,12 @@ class _Pro4WindowsAppState extends State<Pro4WindowsApp> {
               addrFileName: kAddrFileName,
               agentLauncher: launch,
               clientFactory: defaultClient,
-              onClient: _onClient,
+              onClient: (client) async {
+                registerServiceInstance<AgentApiClient>(client);
+                final subscriptionInfo =
+                    context.read<ValueNotifier<SubscriptionInfo>>();
+                subscriptionInfo.value = await client.subscriptionInfo();
+              },
             ),
             child: const StartupPage(nextRoute: Routes.subscriptionStatus),
           ),
@@ -53,11 +50,6 @@ class _Pro4WindowsAppState extends State<Pro4WindowsApp> {
         ),
       ),
     );
-  }
-
-  Future<void> _onClient(AgentApiClient client) async {
-    registerServiceInstance<AgentApiClient>(client);
-    _subscriptionInfo.value = await client.subscriptionInfo();
   }
 }
 
