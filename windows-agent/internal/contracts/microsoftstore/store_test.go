cf0986900cb2d8bad0b34fbf69d60199845bae7f
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-08-25 12:51:22 +0200

    Adapt tests to Github

--- windows-agent/internal/contracts/microsoftstore/store_test.go
+++ windows-agent/internal/contracts/microsoftstore/store_test.go
@@ -52,6 +52,11 @@ func TestGenerateUserJWT(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
+			if tc.wantDllErr != microsoftstore.ErrSuccess && isGithubRunner() {
+				// Github runners run on session 0, so the error is a lack of user ID
+				tc.wantDllErr = microsoftstore.ErrNoLocalUser
+			}
+
 			jwt, err := microsoftstore.GenerateUserJWT(tc.token)
 			if tc.wantErr {
 				require.Error(t, err, "SubscriptionToken should not succeed")
@@ -106,4 +111,8 @@ func buildStoreAPI(ctx context.Context) error {
 	log.Infof(ctx, "Built store api DLL")
 
 	return nil
+}
+
+func isGithubRunner() bool {
+	return os.Getenv("GITHUB_WORKFLOW") != ""
 }
