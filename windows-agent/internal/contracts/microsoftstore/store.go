fb5aff583bca9af5e4d6dd7c6e64da4a704d50c0
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-08-25 12:02:37 +0200

    Implement DLL function calls

--- windows-agent/internal/contracts/microsoftstore/store.go
+++ windows-agent/internal/contracts/microsoftstore/store.go
@@ -1,21 +1,97 @@
-// Package microsoftstore is a stub to implement the contract dance.
 package microsoftstore
 
 import (
-	"context"
 	"errors"
+	"fmt"
+	"sync"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-// Store is a stub type to be replaced with a real implementation.
-type Store struct{}
+var (
+	// Loading must be delayed for tests: the path to the DLL is known only relative to this file.
+	// At module load-time, the working directory can be anywhere.
+	// During the tests, the working directory is at a known location.
+	dll   = syscall.NewLazyDLL("")
+	dllMu sync.Mutex
 
-// New creates a new client for the Microsoft Store.
-func New() *Store {
-	return &Store{}
+	generateUserJWT               = dll.NewProc("GenerateUserJWT")
+	getSubscriptionExpirationDate = dll.NewProc("GetSubscriptionExpirationDate")
+)
+
+// GenerateUserJWT takes an azure AD server access token and returns a Windows store token.
+func GenerateUserJWT(azureADToken string) (string, error) {
+	accessToken, err := syscall.BytePtrFromString(azureADToken)
+	if err != nil {
+		return "", errors.New("could not convert the AzureAD token to a byte array")
+	}
+
+	var userJWTbegin *byte
+	var userJWTlen uint64
+
+	//nolint:gosec // No other way of calling a Dll proc
+	if _, err = call(
+		generateUserJWT,
+		uintptr(unsafe.Pointer(accessToken)),
+		uintptr(unsafe.Pointer(&userJWTbegin)),
+		uintptr(unsafe.Pointer(&userJWTlen)),
+	); err != nil {
+		return "", err
+	}
+
+	//nolint:gosec // This is the way of freeing userJWTbegin per storeapi's API definition
+	defer windows.CoTaskMemFree(unsafe.Pointer(userJWTbegin))
+
+	return string(unsafe.Slice(userJWTbegin, userJWTlen)), nil
 }
 
-// SubscriptionToken retrieves a JWT token for the application associated with
-// the provided azureADToken.
-func (*Store) SubscriptionToken(ctx context.Context, azureADToken string) (string, error) {
-	return "", errors.New("the Microsoft Store interaction is not yet implemented")
+// call forces the proc and DLL to load before calling it, and cleans up the output.
+// Use this instead of proc.Call to avoid panics.
+//
+//nolint:unparam // Return value is provided to follow convention.
+func call(proc *syscall.LazyProc, args ...uintptr) (int, error) {
+	if err := loadDll(); err != nil {
+		return 0, err
+	}
+
+	// Avoid panic in Call by calling Find beforehand.
+	if err := proc.Find(); err != nil {
+		return 0, err
+	}
+
+	hresult, _, err := proc.Call(args...)
+	if err != nil && !errors.Is(err, syscall.Errno(0)) {
+		return hresult, fmt.Errorf("%s: %v", proc.Name, err)
+	}
+
+	if err := NewStoreAPIError(hresult); err != nil {
+		return hresult, fmt.Errorf("%s returned error code %d: %w", generateUserJWT.Name, hresult, err)
+	}
+
+	return hresult, nil
+}
+
+// loadDll finds the dll and ensures it loads.
+func loadDll() error {
+	dllMu.Lock()
+	defer dllMu.Unlock()
+
+	if dll.Name != "" {
+		return nil
+	}
+
+	path, err := locateStoreDll()
+	if err != nil {
+		return fmt.Errorf("could not find Windows Store API dll: %v", err)
+	}
+
+	dll.Name = path
+	if err = dll.Load(); err != nil {
+		dll.Name = ""
+		return err
+	}
+
+	return nil
 }
---
+++ windows-agent/internal/contracts/microsoftstore/store_test.go
@@ -0,0 +1,55 @@
+package microsoftstore_test
+
+import (
+	"testing"
+
+	"github.com/canonical/ubuntu-pro-for-windows/windows-agent/internal/contracts/microsoftstore"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	if runtime.GOOS == "windows" {
+		if err := buildStoreAPI(ctx); err != nil {
+			fmt.Fprintf(os.Stderr, "Setup: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	exit := m.Run()
+	defer os.Exit(exit)
+}
+
+func TestGenerateUserJWT(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		token string
+
+		wantErr    bool
+		wantDllErr microsoftstore.StoreAPIError
+	}{
+		"Error because there is no subscription":       {token: "not a real token", wantErr: true, wantDllErr: microsoftstore.ErrStoreAPI},
+		"Error because the token has a null character": {token: "invalid \x00 token", wantErr: true},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			jwt, err := microsoftstore.GenerateUserJWT(tc.token)
+			if tc.wantErr {
+				require.Error(t, err, "SubscriptionToken should not succeed")
+				if tc.wantDllErr != 0 {
+					require.ErrorIs(t, err, tc.wantDllErr, "SubscriptionToken returned an unexpected error type")
+				}
+				return
+			}
+
+			require.NoError(t, err, "SubscriptionToken should succeed")
+			require.NotEmpty(t, jwt, "User JWToken should not be empty")
+		})
+	}
+}
