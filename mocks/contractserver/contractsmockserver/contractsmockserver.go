26c424bc6f6d0cc77dbe88cdb520f5413e7c4ec0
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-09-08 03:08:28 -0300

    Updates the contracts mock
    I managed to sneak a little bit of OOP into Go :)
    
    contractsmockserver main business now is its own initialization
    which requires creating a ServeMux and handling the requests that manage
    to pass through validation.
    
    Boilerplate was "inherited" from ServerBase.

--- mocks/contractserver/contractsmockserver/contractsmockserver.go
+++ mocks/contractserver/contractsmockserver/contractsmockserver.go
@@ -3,18 +3,13 @@
 package contractsmockserver
 
 import (
-	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log/slog"
-	"net"
 	"net/http"
 	"path"
-	"sync"
-	"time"
 
 	"github.com/canonical/ubuntu-pro-for-windows/contractsapi"
+	"github.com/canonical/ubuntu-pro-for-windows/mocks/restserver"
 )
 
 const (
@@ -28,89 +23,49 @@ const (
 
 // Server is a mock of the contract server, where its behaviour can be modified.
 type Server struct {
+	restserver.ServerBase
 	settings Settings
-
-	server *http.Server
-	mu     sync.RWMutex
-
-	done chan struct{}
 }
 
 // Settings contains the parameters for the Server.
 type Settings struct {
-	Token        Endpoint
-	Subscription Endpoint
+	Token        restserver.Endpoint
+	Subscription restserver.Endpoint
 	Address      string
 }
 
-// Endpoint contains settings for an API endpoint behaviour. Can be modified for testing purposes.
-type Endpoint struct {
-	// OnSuccess is the response returned in the happy path.
-	OnSuccess Response
-
-	// Disabled disables the endpoint.
-	Disabled bool
-
-	// Blocked means that a response will not be sent back, instead it'll block until the server is stopped.
-	Blocked bool
+// GetAddress returns the previously set address.
+func (s *Settings) GetAddress() string {
+	return s.Address
 }
 
-// Response contains settings for an API endpoint response behaviour. Can be modified for testing purposes.
-type Response struct {
-	Value  string
-	Status int
+// SetAddress updates a Settings object with the new address.
+func (s *Settings) SetAddress(addr string) {
+	s.Address = addr
 }
 
 // DefaultSettings returns the default set of settings for the server.
 func DefaultSettings() Settings {
 	return Settings{
-		Token:        Endpoint{OnSuccess: Response{Value: DefaultADToken, Status: http.StatusOK}},
-		Subscription: Endpoint{OnSuccess: Response{Value: DefaultProToken, Status: http.StatusOK}},
+		Token:        restserver.Endpoint{OnSuccess: restserver.Response{Value: DefaultADToken, Status: http.StatusOK}},
+		Subscription: restserver.Endpoint{OnSuccess: restserver.Response{Value: DefaultProToken, Status: http.StatusOK}},
 		Address:      "localhost:0",
 	}
 }
 
 // NewServer creates a new contract server with the provided settings.
 func NewServer(s Settings) *Server {
-	return &Server{
-		settings: s,
+	sv := &Server{
+		ServerBase: restserver.ServerBase{GetAddress: s.GetAddress},
+		settings:   s,
 	}
+	sv.Mux = sv.NewMux()
+
+	return sv
 }
 
-// Stop stops the server.
-func (s *Server) Stop() error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if s.server == nil {
-		return errors.New("already stopped")
-	}
-
-	err := s.server.Close()
-	<-s.done
-
-	s.server = nil
-
-	return err
-}
-
-// Serve starts a new HTTP server mocking the Contracts Server backend REST API with
-// responses defined according to Server Settings. Use Stop to Stop the server and
-// release resources.
-func (s *Server) Serve(ctx context.Context) (string, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if s.server != nil {
-		return "", errors.New("already serving")
-	}
-
-	var lc net.ListenConfig
-	lis, err := lc.Listen(ctx, "tcp", s.settings.Address)
-	if err != nil {
-		return "", fmt.Errorf("failed to listen over tcp: %v", err)
-	}
-
+// NewMux sets up a ServeMux to handle the server endpoints enabled according to the server settings.
+func (s *Server) NewMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	if !s.settings.Token.Disabled {
@@ -121,27 +76,12 @@ func (s *Server) Serve(ctx context.Context) (string, error) {
 		mux.HandleFunc(path.Join(contractsapi.Version, contractsapi.SubscriptionPath), s.handleSubscription)
 	}
 
-	s.server = &http.Server{
-		Addr:              lis.Addr().String(),
-		Handler:           mux,
-		ReadHeaderTimeout: 3 * time.Second,
-	}
-
-	s.done = make(chan struct{})
-
-	go func() {
-		defer close(s.done)
-		if err := s.server.Serve(lis); err != nil && err != http.ErrServerClosed {
-			slog.Error("Failed to start the HTTP server", "error", err)
-		}
-	}()
-
-	return lis.Addr().String(), nil
+	return mux
 }
 
-// handleToken implements the /token endpoint.
+// handleToken implements the /token restserver.Endpoint.
 func (s *Server) handleToken(w http.ResponseWriter, r *http.Request) {
-	if err := s.handle(w, r, http.MethodGet, s.settings.Token); err != nil {
+	if err := s.ValidateRequest(w, r, http.MethodGet, s.settings.Token); err != nil {
 		fmt.Fprintf(w, "%v", err)
 		return
 	}
@@ -153,9 +93,9 @@ func (s *Server) handleToken(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// handleSubscription implements the /susbcription endpoint.
+// handleSubscription implements the /susbcription restserver.Endpoint.
 func (s *Server) handleSubscription(w http.ResponseWriter, r *http.Request) {
-	if err := s.handle(w, r, http.MethodPost, s.settings.Subscription); err != nil {
+	if err := s.ValidateRequest(w, r, http.MethodPost, s.settings.Subscription); err != nil {
 		fmt.Fprintf(w, "%v", err)
 		return
 	}
@@ -185,32 +125,4 @@ func (s *Server) handleSubscription(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "failed to write the response: %v", err)
 		return
 	}
-}
-
-// handle extracts common boilerplate from endpoints.
-func (s *Server) handle(w http.ResponseWriter, r *http.Request, wantMethod string, endpoint Endpoint) (err error) {
-	slog.Info("Received request", "endpoint", r.URL.Path, "method", r.Method)
-	defer func() {
-		if err != nil {
-			slog.Error("bad request", "error", err, "endpoint", r.URL.Path, "method", r.Method)
-		}
-	}()
-
-	if r.Method != wantMethod {
-		w.WriteHeader(http.StatusBadRequest)
-		return fmt.Errorf("this endpoint only supports %s", wantMethod)
-	}
-
-	if endpoint.Blocked {
-		<-s.done
-		slog.Debug("Server context was cancelled. Exiting", "endpoint", r.URL.Path)
-		return errors.New("server stopped")
-	}
-
-	if endpoint.OnSuccess.Status != 200 {
-		w.WriteHeader(endpoint.OnSuccess.Status)
-		return fmt.Errorf("mock error: %d", endpoint.OnSuccess.Status)
-	}
-
-	return nil
 }
