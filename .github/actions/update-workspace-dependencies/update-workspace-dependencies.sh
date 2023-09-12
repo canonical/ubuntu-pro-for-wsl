aacc0ee6c9598fec751c025e7a1bbcd8b08c2f9b
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-09-12 13:13:15 +0200

    Passs via command line arguments

--- .github/actions/update-workspace-dependencies/update-workspace-dependencies.sh
+++ .github/actions/update-workspace-dependencies/update-workspace-dependencies.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 set -eu
 
-cd ${TARGET_MODULE}
+github_repo="${1:-""}"
+target_module="${2:-""}"
+
+if [[ -z "${github_repo}" || -z "${target_module}" ]] ; then
+    echo "Update the out-of-date workspace dependencies of the target modules"
+    echo "This script must be run from the workspace root."
+    echo "Usage:"
+    echo "    $(basename $0) github_repo target_module"
+    echo ""
+    echo "github_repo:"
+    echo "    The 'user/repository' of the workspace"
+    echo ""
+    echo "target_module:"
+    echo "    The path of the module relative to the workspace root"
+    exit 2
+fi
+
+cd ${target_module}
 current_commit=$(git rev-parse HEAD~0)
 
 # Find internal dependencies
-repo="github.com/${GITHUB_REPOSITORY}"
-url="${repo}/${TARGET_MODULE}"
+repo="github.com/${github_repo}"
+url="${repo}/${target_module}"
 regex="s#${url} \(${repo}/[^@]\+@v.*\)#\1#p"
 dependencies=`go mod graph | sed -n "${regex}"`
 
-for dependency in ${dependencies}; do  
+for dependency in ${dependencies}; do
     pattern="^${repo}/\(.*\)@v\([^-]\+\)-\([^-]\+\)-\(.*\)$"
     # Capture groups: (https://go.dev/ref/mod#versions)
     # \1 Dependency path
@@ -20,7 +37,7 @@ for dependency in ${dependencies}; do
 
     dep_path=`echo $dependency | sed "s#${pattern}#\1#"`
     dep_commit=`echo $dependency | sed "s#${pattern}#\4#"`
-    
+
     diff_files="$(git diff --name-only ${dep_commit} -- "../${dep_path}")"
     if [ -z "${diff_files}" ] ; then
         continue
