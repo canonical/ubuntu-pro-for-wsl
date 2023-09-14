759ba7ebbe56899faf3f054eb60959885df88a04
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-09-14 13:50:37 -0300

    Purchase enum and callback goes to its own file
    The enum is platform agnostic.
    The callback type declaration as well.
    Both will be required in different places.
    The translate function is specific to WinRT.
    Thus, it will be restored in the specific context impl later.

---
+++ storeapi/base/Purchase.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+/// Types and aliases that must be present in all Context implementations.
+
+#include <cstdint>
+#include <functional>
+
+namespace StoreApi {
+// We'll certainly want to show in the UI the result of the purchase operation
+// in a localizable way. Thus we must agree on the values returned across the
+// different languages involved. Since we don't control the Windows Runtime
+// APIs, it wouldn't be future-proof to return the raw value of
+// StorePurchaseStatus enum right away.
+// This must strictly in sync the Dart PurchaseStatus enum in
+// https://github.com/canonical/ubuntu-pro-for-windows/blob/main/gui/packages/p4w_ms_store/lib/p4w_ms_store_platform_interface.dart#L8-L16
+// so we don't misinterpret the native call return values.
+enum class PurchaseStatus : std::int8_t {
+  Succeeded = 0,
+  AlreadyPurchased = 1,
+  UserGaveUp = 2,
+  NetworkError = 3,
+  ServerError = 4,
+  Unknown = 5,
+};
+
+/// A callable the client application must provide to receive the result of the
+/// asynchronous purchase operation.
+using PurchaseCallback = std::function<void(PurchaseStatus, std::int32_t)>;
+
+}  // namespace StoreApi
--- storeapi/gui/ClientStoreService.hpp
+++ storeapi/gui/ClientStoreService.hpp
@@ -3,42 +3,6 @@
 #include <base/StoreService.hpp>
 
 namespace StoreApi {
-
-// We'll certainly want to show in the UI the result of the purchase operation
-// in a localizable way. Thus we must agree on the values returned across the
-// different languages involved. Since we don't control the Windows Runtime
-// APIs, it wouldn't be future-proof to return the raw value of
-// StorePurchaseStatus enum right away.
-// This must strictly in sync the Dart PurchaseStatus enum in
-// https://github.com/canonical/ubuntu-pro-for-windows/blob/main/gui/packages/p4w_ms_store/lib/p4w_ms_store_platform_interface.dart#L8-L16
-// so we don't misinterpret the native call return values.
-enum class PurchaseStatus : std::int8_t {
-  Succeeded = 0,
-  AlreadyPurchased = 1,
-  UserGaveUp = 2,
-  NetworkError = 3,
-  ServerError = 4,
-  Unknown = 5,
-};
-
-PurchaseStatus translate(
-    winrt::Windows::Services::Store::StorePurchaseStatus purchaseStatus) {
-  using winrt::Windows::Services::Store::StorePurchaseStatus;
-  switch (purchaseStatus) {
-    case StorePurchaseStatus::Succeeded:
-      return PurchaseStatus::Succeeded;
-    case StorePurchaseStatus::AlreadyPurchased:
-      return PurchaseStatus::AlreadyPurchased;
-    case StorePurchaseStatus::NotPurchased:
-      return PurchaseStatus::UserGaveUp;
-    case StorePurchaseStatus::NetworkError:
-      return PurchaseStatus::NetworkError;
-    case StorePurchaseStatus::ServerError:
-      return PurchaseStatus::ServerError;
-  }
-  return PurchaseStatus::Unknown; // To be future proof.
-}
-
 // Adds functionality on top of the [StoreService] interesting to Client UI
 // applications.
 template <typename ContextType = Context>
