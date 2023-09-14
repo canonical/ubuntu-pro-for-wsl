2ec64438f1b1c44f574a363919c6cb69f5883860
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-09-14 13:43:10 -0300

    The code we want to write at the high level
    The purchase method must now be callback based
    That means the result will be provided to the callback
    That implies the method channel `result` must be a shared_ptr
    Since we may need to access it in the exception handler as well.
    
    Since fetching a product will be blocking
    we need to call it on a background thread
    Purchasing needs to start on the main (UI) thread, though
    
    All exceptions are captured.
    
    Note: The DefaultContext header must select what Context impl tol use.
    More headers being included to be pedantic (don't accept transitive)
    Rule: don't #include in cpp what my component header provides directly.

--- gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin_impl.cpp
+++ gui/packages/p4w_ms_store/windows/p4w_ms_store_plugin_impl.cpp
@@ -1,26 +1,44 @@
 #include "p4w_ms_store_plugin_impl.h"
 
-#include <flutter/encodable_value.h>
-
+#include <base/DefaultContext.hpp>
+#include <base/Exception.hpp>
+#include <base/Purchase.hpp>
 #include <gui/ClientStoreService.hpp>
+
+#include <cstdint>
+#include <exception>
 
 namespace p4w_ms_store {
 
 winrt::fire_and_forget PurchaseSubscription(
     HWND topLevelWindow, std::string productId,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  try {
-    StoreApi::ClientStoreService service{topLevelWindow};
-    const auto res =
-        co_await service.PromptUserToSubscribe(std::move(productId));
-    result->Success(static_cast<int>(res));
-  } catch (StoreApi::Exception& err) {
-    result->Error(channelName, err.what());
-  } catch (winrt::hresult_error& err) {
-    result->Error(channelName, winrt::to_string(err.message()));
-  } catch (std::exception& err) {
-    result->Error(channelName, err.what());
-  }
+    std::shared_ptr<flutter::MethodResult<flutter::EncodableValue>>
+        result) try {
+  winrt::apartment_context windowContext{};
+  StoreApi::ClientStoreService service{topLevelWindow};
+  // Blocks a background thread while retrieving the product.
+  co_await winrt::resume_background();
+  const auto product = service.FetchAvailableProduct(productId);
+  // Resumes in the UI thread to display the native dialog.
+  co_await windowContext;
+  product.PromptUserForPurchase(
+      [result](StoreApi::PurchaseStatus status, int32_t error) {
+        if (error < 0) {
+          winrt::hresult_error err{error};
+          result->Error(channelName, winrt::to_string(err.message()));
+          return;
+        }
+
+        result->Success(static_cast<int>(status));
+      });
+} catch (StoreApi::Exception& err) {
+  result->Error(channelName, err.what());
+} catch (winrt::hresult_error& err) {
+  result->Error(channelName, winrt::to_string(err.message()));
+} catch (std::exception& err) {
+  result->Error(channelName, err.what());
+} catch (...) {
+  result->Error(channelName, "Unknown exception thrown in the native layer.");
 }
 
 }  // namespace p4w_ms_store
