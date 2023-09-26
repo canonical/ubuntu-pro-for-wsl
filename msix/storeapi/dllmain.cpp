63c6867368c6e605c17ebbea1649b169e72b489c
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-09-26 15:30:39 +0200

    Avoid cerr stuttering
    This also considerably simplifies the code. You may be concerned that I
    no longer print the reportType. That is because the message already
    contains it.

--- msix/storeapi/dllmain.cpp
+++ msix/storeapi/dllmain.cpp
@@ -1,36 +1,19 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+#include <stdexcept>
+
 #include "framework.hpp"
 
 #ifdef _MSC_VER
 #include <Windows.h>
-
-#include <iostream>
-#include <string_view>
-
-int DebugReportHook(int reportType, char* message, int* returnValue) {
-  const auto type = [=]() -> std::string_view {
-    switch (reportType) {
-      case _CRT_WARN:
-        return "[WARNING]";
-      case _CRT_ERROR:
-        return "[ERROR]";
-      case _CRT_ASSERT:
-        return "[ASSERT]";
-      default:
-        return "[UNKNOWN]";
-    }
-  }();
-
-  std::cerr << type << ' ' << message << std::endl;
-  throw std::runtime_error(message);
-}
 #else
-#define _CrtSetReportHook(x)
+constexpr void _CrtSetReportHook(auto) {}
 #endif  // _MSC_VER
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
                       LPVOID lpReserved) {
-  _CrtSetReportHook(DebugReportHook);
+  _CrtSetReportHook([](int reportType, char* message, int* returnValue) -> int {
+    throw std::runtime_error(message);
+  });
 
   switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
