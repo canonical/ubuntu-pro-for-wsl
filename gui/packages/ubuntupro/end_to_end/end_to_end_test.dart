ed38b26f0ecb9025a8a94420f551e4d054229bcd
Carlos <carlos.santanadeoliveira@canonical.com>
2023-10-04 01:59:28 -0300

    Implements the Flutter automation for e2e
    Assertions should be minimal.
    Only one test scenario must be active for the lifetime of the process.
    The CLI arg[0] determines what scenario to activate.
    That matches the test function name on the Go side.
    There is where the assertions must exist.
    There we assert the side effects on registry, distros etc.
    This is merely a script (compiled to machine code, ofc)

---
+++ gui/packages/ubuntupro/end_to_end/end_to_end_test.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:stack_trace/stack_trace.dart' as stack_trace;
+import 'package:ubuntupro/core/environment.dart';
+import 'package:ubuntupro/main.dart' as app;
+import 'package:ubuntupro/pages/subscription_status/subscribe_now_page.dart';
+import 'package:ubuntupro/pages/subscription_status/subscription_status_page.dart';
+
+import '../test/utils/l10n_tester.dart';
+
+const proTokenEnv = 'UP4W_TEST_PRO_TOKEN';
+
+typedef TestCases = Map<String, Future<void> Function(WidgetTester)>;
+
+void main(List<String> args) {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  FlutterError.demangleStackTrace = (stack) {
+    if (stack is stack_trace.Trace) return stack.vmTrace;
+    if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
+    return stack;
+  };
+
+  const testCases = {
+    'TestOrganizationProvidedToken': testOrganizationProvidedToken,
+    'TestManualTokenInput': testManualTokenInput,
+  };
+
+  final scenario = args[0];
+
+  if (!testCases.keys.contains(scenario)) {
+    debugPrint('"$scenario" is not a valid test scenario.');
+    exit(1);
+  }
+
+  // For a single run of the end-to-end tests we can only have one active GUI test scenario,
+  // what implies that we must define a single 'testWidgets'. The actual function body will be determined at runtime.
+  testWidgets(
+    scenario,
+    testCases[scenario]!,
+    skip: !Platform.isWindows,
+  );
+}
+
+Future<void> testOrganizationProvidedToken(WidgetTester tester) async {
+  await app.main();
+  await tester.pumpAndSettle();
+
+  // asserts that we transitioned to the organization-managed status page.
+  final l10n = tester.l10n<SubscriptionStatusPage>();
+  expect(find.text(l10n.orgManaged), findsOneWidget);
+}
+
+Future<void> testManualTokenInput(WidgetTester tester) async {
+  await app.main();
+  await tester.pumpAndSettle();
+
+  // The "subscribe now page" is only shown if the GUI communicates with the background agent.
+  var l10n = tester.l10n<SubscribeNowPage>();
+
+  // expands the collapsed input field group
+  final toggle = find.byType(IconButton);
+  await tester.tap(toggle);
+  await tester.pumpAndSettle();
+
+  // finds the pro token from the environment
+  final goodToken = Environment()[proTokenEnv];
+  expect(
+    goodToken,
+    isNotNull,
+    reason: '$proTokenEnv must be set to a valid token.',
+  );
+
+  // enters a good token value
+  final inputField = find.byType(TextField);
+  await tester.enterText(inputField, goodToken!);
+  await tester.pumpAndSettle();
+
+  // submits the input.
+  final button = find.text(l10n.apply);
+  await tester.tap(button);
+  await tester.pumpAndSettle();
+
+  // asserts that we transitioned to the user-managed status page.
+  l10n = tester.l10n<SubscriptionStatusPage>();
+  expect(find.text(l10n.manuallyManaged), findsOneWidget);
+}
