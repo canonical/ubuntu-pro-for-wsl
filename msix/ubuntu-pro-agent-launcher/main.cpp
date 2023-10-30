82c9b05d2a18e3a7b0ef47980e07edce0c958468
Carlos <carlos.santanadeoliveira@canonical.com>
2023-10-30 01:49:05 -0300

    Adds the launcher vcxproj to the msix solution

---
+++ msix/ubuntu-pro-agent-launcher/main.cpp
@@ -0,0 +1,8 @@
+/// A Windows Application that creates an invisible pseudo-console to host the ubuntu-pro-agent.exe;
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine, int nCmdShow)
+{
+  return 0;
+}
