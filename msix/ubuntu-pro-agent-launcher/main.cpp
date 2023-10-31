93d65090b33970486ad85563af89c583884a45e4
Carlos <carlos.santanadeoliveira@canonical.com>
2023-10-30 23:28:06 -0300

    The main I want to write
    GUI applications rely on some sort of event loop to mediate the
    unpredictable timing user input will happen.
    
    The Windows OS models this by means of the window message loop
    and the WndProc function that says how the window responds to
    messages it receives.
    
    We want to wait on ReadFile to drain the console pipes, so the
    child process won't block on the next printf after the pipe becomes
    full.
    We also want to wait on the agent process so we can quit if it quits.
    We also want to respond to messages sent by the OS (such as WM_QUIT).
    
    Threads without actual windows still have the message loop.
    We can combine the window approach with some waiting functions and
    create a custom message loop that allows our thread to sleep while
    we don't receive any relevant input neither on the thread queue,
    nor on the console pipe or the agent process termination.
    
    Creating the pseudo-console should be declarative.
    Starting the agent process under the pseudo-console should be a function
    call.
    Reacting to the agent's process termination or new input from the
    console pipe should resemble reactive programming by passing functions.
    
    I don't want to see tons of Win32 in my main()
    
    I hope you enjoy this small spec.
    
    :)

--- msix/ubuntu-pro-agent-launcher/main.cpp
+++ msix/ubuntu-pro-agent-launcher/main.cpp
@@ -1,8 +1,83 @@
-/// A Windows Application that creates an invisible pseudo-console to host the ubuntu-pro-agent.exe;
+/// A Windows Application that creates an invisible pseudo-console to host the
+/// ubuntu-pro-agent.exe;
 #include <windows.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                    PWSTR pCmdLine, int nCmdShow)
-{
-  return 0;
+#include <array>
+#include <cstddef>
+#include <exception>
+#include <format>
+#include <optional>
+
+#include "console.hpp"
+#include "error.hpp"
+
+std::filesystem::path const& logPath() {
+  static std::filesystem::path localAppDataPath =
+      up4w::MakePathRelativeToEnvDir(
+          L"\\Ubuntu Pro\\ubuntu-pro-agent-launcher.log", L"LOCALAPPDATA");
+  return localAppDataPath;
+}
+
+std::filesystem::path thisBinaryDir() {
+  wchar_t binaryPath[MAX_PATH];
+  DWORD fnLength = GetModuleFileName(nullptr, binaryPath, MAX_PATH);
+  if (fnLength == 0) {
+    return std::filesystem::path();
+  }
+  std::filesystem::path exePath{std::wstring_view{binaryPath, fnLength}};
+  exePath.remove_filename();
+
+  return exePath;
+}
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR pCmdLine, int) try {
+  // setup the app: pipes and console
+  up4w::PseudoConsole console{{.X = 80, .Y = 80}};
+
+  // start the child process
+  auto agent = thisBinaryDir() / L"ubuntu-pro-agent.exe";
+  auto p = console.StartProcess(std::format(L"{} {}", agent.c_str(), pCmdLine));
+
+  // setup the event loop with listeners.
+  up4w::EventLoop ev{{
+                         p.hProcess,
+                         [](HANDLE process) {
+                           DWORD exitCode = 0;
+                           GetExitCodeProcess(process, &exitCode);
+                           return exitCode;
+                         },
+                     },
+                     {
+                         console.GetReadHandle(),
+                         [](HANDLE read) {
+                           std::array<std::byte, 256> buffer{};
+                           DWORD bytesRead = 0;
+                           ReadFile(read, &buffer[0],
+                                    static_cast<DWORD>(buffer.size() - 1),
+                                    &bytesRead, nullptr);
+                           return std::nullopt;
+                         },
+                     }};
+
+  // dispatch the event loop
+  return ev.Run();
+
+  // log errors, if any.
+} catch (up4w::hresult_exception const& err) {
+  std::filesystem::path const& localAppDataPath = logPath();
+  if (localAppDataPath.empty()) {
+    return 1;
+  }
+
+  auto msg = std::format("{}\n\t{}", err.message().c_str(), err.where());
+  up4w::LogSingleShot(localAppDataPath, msg);
+  return 2;
+} catch (std::exception const& err) {
+  std::filesystem::path const& localAppDataPath = logPath();
+  if (localAppDataPath.empty()) {
+    return 1;
+  }
+
+  up4w::LogSingleShot(localAppDataPath, err.what());
+  return 3;
 }
