7fce844c62d6b3c1c5d891f333de62128e7ed9ad
Carlos <carlos.santanadeoliveira@canonical.com>
2023-10-31 00:04:48 -0300

    Implements StartProcess
    By far the most complicated part of this feature.
    Starting a child process under a pseudo console requires:
    1. a STARTUPINFOEX structure filled with the handles the pseudo console
       uses for stdio
    2. a LPPROC_THREAD_ATTRIBUTE_LIST containing the key PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE
       mapped to the HPCON (pseudo-console device handle).
       This list has to be allocated in the heap, since the size of its
       elements is hidden from the application code.
       We need to try to initialize it first to fail deterministically, just
       to learn how many bytes needs to be allocated.
       Then allocate the required amount of memory and try again, which
       should succeed.
       Then we fill the key-value pair we desire by calling
       UpdateProcThreadAttribute.
       Notice that's not related to the current process or thread,
       but for the one we'll start.
    3. Then we CreateProcessW
    4. Care must be taken to avoid leaking the process/thread attribute
       list.
       Disposing it requires calling DeleteProcThreadAttributeList followed
       by freeing the memory.
       Again, the unique_ptr with custom deleter idiom comes in handy.
       This is scoped to the process creation, once done we no longer need
       such structure.
       Since this program is meant to start only one process and live in
       sync with it, we don't need to either cache that thing nor make it
       visible outside of this routine.

--- msix/ubuntu-pro-agent-launcher/console.cpp
+++ msix/ubuntu-pro-agent-launcher/console.cpp
@@ -1,5 +1,8 @@
 #include "console.hpp"
 
+#include <memory>
+#include <type_traits>
+
 #include "error.hpp"
 
 namespace up4w {
@@ -35,6 +38,66 @@ PseudoConsole::PseudoConsole(COORD coordinates) {
       FAILED(hr)) {
     throw hresult_exception{hr};
   }
+}
+
+void attr_list_deleter(PPROC_THREAD_ATTRIBUTE_LIST p) {
+  if (p) {
+    DeleteProcThreadAttributeList(p);
+    HeapFree(GetProcessHeap(), 0, p);
+  }
+};
+using unique_attr_list =
+    std::unique_ptr<std::remove_pointer_t<PPROC_THREAD_ATTRIBUTE_LIST>,
+                    decltype(&attr_list_deleter)>;
+
+/// Returns a list of attributes for process/thread creation with the
+/// pseudo-console key enabled and set to [con].
+unique_attr_list PseudoConsoleProcessAttrList(HPCON con) {
+  PPROC_THREAD_ATTRIBUTE_LIST attrs = nullptr;
+
+  size_t bytesRequired = 0;
+  InitializeProcThreadAttributeList(NULL, 1, 0, &bytesRequired);
+  // Allocate memory to represent the list
+  attrs = static_cast<PPROC_THREAD_ATTRIBUTE_LIST>(
+      HeapAlloc(GetProcessHeap(), 0, bytesRequired));
+  if (!attrs) {
+    throw hresult_exception{E_OUTOFMEMORY};
+  }
+
+  // Initialize the list memory location
+  if (!InitializeProcThreadAttributeList(attrs, 1, 0, &bytesRequired)) {
+    throw hresult_exception{HRESULT_FROM_WIN32(GetLastError())};
+  }
+
+  unique_attr_list result{attrs, &attr_list_deleter};
+
+  if (!UpdateProcThreadAttribute(attrs, 0, PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE,
+                                 con, sizeof(con), NULL, NULL)) {
+    throw hresult_exception{HRESULT_FROM_WIN32(GetLastError())};
+  }
+
+  return result;
+}
+
+Process PseudoConsole::StartProcess(std::wstring commandLine) {
+  unique_attr_list attributes = PseudoConsoleProcessAttrList(hDevice);
+  // Prepare Startup Information structure
+  STARTUPINFOEX si{};
+  si.StartupInfo.cb = sizeof(STARTUPINFOEX);
+  si.StartupInfo.hStdInput = hInRead;
+  si.StartupInfo.hStdOutput = hOutWrite;
+  si.StartupInfo.hStdError = hOutWrite;
+  si.StartupInfo.dwFlags = STARTF_USESTDHANDLES;
+  si.lpAttributeList = attributes.get();
+
+  Process p{0};
+  if (!CreateProcessW(NULL, commandLine.data(), NULL, NULL, FALSE,
+                      EXTENDED_STARTUPINFO_PRESENT, NULL, NULL, &si.StartupInfo,
+                      &p)) {
+    throw hresult_exception{HRESULT_FROM_WIN32(GetLastError())};
+  }
+
+  return p;
 }
 
 }  // namespace up4w
