d3fec554400b2a0e261e0dd27ed5968ad2e40041
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-12-14 17:05:00 +0100

    Implement mock registry
    It got much more complex because we now need to signal notifiers every
    time a key, or any of its children, are modified.

--- windows-agent/internal/proservices/registrywatcher/registry/registry_linux.go
+++ windows-agent/internal/proservices/registrywatcher/registry/registry_linux.go
@@ -3,17 +3,34 @@ package registry
 // Windows is the Windows registry. Any interaction with it will panic.
 type Windows struct{}
 
-// HKCUOpenKey opens a key in the specified path under the HK_CURRENT_USER registry.
-func (Windows) HKCUOpenKey(path string) (key uintptr, err error) {
+// HKCUOpenKey opens a key in the specified path under the HK_CURRENT_USER registry with read permissions.
+func (Windows) HKCUOpenKey(path string) (Key, error) {
 	panic("the Windows registry is not available on Linux")
 }
 
 // CloseKey releases a key.
-func (Windows) CloseKey(k uintptr) {
+func (Windows) CloseKey(k Key) {
 	panic("the Windows registry is not available on Linux")
 }
 
 // ReadValue returns the value of the specified field in the specified key.
-func (Windows) ReadValue(k uintptr, field string) (value string, err error) {
+func (Windows) ReadValue(k Key, field string) (string, error) {
+	panic("the Windows registry is not available on Linux")
+}
+
+// RegNotifyChangeKeyValue creates an event and attaches it to a registry key.
+// Modifying that key or its children will trigger the event.
+// This trigger can be detected by WaitSingleObject.
+func (Windows) RegNotifyChangeKeyValue(k Key) (ev Event, err error) {
+	panic("the Windows registry is not available on Linux")
+}
+
+// WaitForSingleObject waits until the event is triggered. This is a blocking function.
+func (Windows) WaitForSingleObject(ev Event) (err error) {
+	panic("the Windows registry is not available on Linux")
+}
+
+// CloseEvent releases the event.
+func (Windows) CloseEvent(ev Event) {
 	panic("the Windows registry is not available on Linux")
 }
--- windows-agent/internal/proservices/registrywatcher/registry/registry_mock.go
+++ windows-agent/internal/proservices/registrywatcher/registry/registry_mock.go
@@ -1,82 +1,330 @@
 package registry
 
 import (
+	"context"
 	"errors"
-	"sync/atomic"
+	"math/rand"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // Mock is a fake registry stored in memory.
-// It can only have one key:
-//
-//	HKCU\Software\Canonical\UbuntuPro
 type Mock struct {
-	// UbuntuProData contains the values stored in the UbuntuPro key.
-	UbuntuProData map[string]string
+	// registry contains the registry key database.
+	registry map[string]*key
 
-	// KeyExists indicates whether the UbuntuPro key already exists in the registry.
-	KeyExists bool
+	// keyHandles contains the handles to the keys. The Win32API returns void pointers to the
+	// key handles, and we mimic this behaviour so we can fit the interface. The user of this
+	// library will have a "pointer", which is just a key into this map.
+	keyHandles mockedHeap[Key, *keyHandle]
 
-	// Error triggers so the tests can mock system errors
-	Errors uint32
-
-	// OpenKeyCount can be used by tests to ensure they don't leak by asserting this is 0 at the end of the test
-	OpenKeyCount *atomic.Int32
+	// eventsHandles contains the eventsHandles. The Win32API returns void pointers to the eventsHandles, and we
+	// mimic this behaviour so we can fit the interface. The user of this  library will have
+	// a "pointer", which is just a key into this map.
+	eventHandles mockedHeap[Event, *eventHandle]
 }
 
-// Error triggers so the tests can mock system errors.
-const (
-	MockErrOnCreateKey uint32 = 1 << iota
-	MockErrOnOpenKey
-	MockErrReadValue
-	MockErrOnWriteValue
-)
+// key mocks a registry key.
+type key struct {
+	mu     *sync.RWMutex
+	parent *key
+	data   map[string]string
+	events []Event
+}
+
+func (r *Mock) notify(k *key) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+
+	// Trigger all events
+	r.eventHandles.mu.Lock()
+	for _, event := range k.events {
+		if e, ok := r.eventHandles.data[event]; ok {
+			e.trigger()
+		}
+	}
+	r.eventHandles.mu.Unlock()
+
+	// Reset the list
+	k.events = make([]Event, 0)
+
+	// Notify parents
+	if k.parent != nil {
+		r.notify(k.parent)
+	}
+}
+
+func (r *Mock) setValue(k *key, field, value string) {
+	defer r.notify(k)
+
+	k.mu.Lock()
+	defer k.mu.Unlock()
+
+	k.data[field] = value
+}
+
+func (*Mock) getValue(k *key, field string) (string, error) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+
+	d, ok := k.data[field]
+	if !ok {
+		return d, ErrFieldNotExist
+	}
+
+	return d, nil
+}
+
+// keyHandle represents the object Win32 callers get when opening a key.
+// Note that the Win32 API returns a HANDLE (i.e. a typedef'd void*), so this
+// struct represents the structure that HANDLE points to.
+type keyHandle struct {
+	key      *key
+	readOnly bool
+
+	ctx       context.Context
+	cancelCtx context.CancelFunc
+}
+
+// eventHandle represents the object Win32 callers get when creating an event.
+// Note that the Win32 API returns a HANDLE (i.e. a typedef'd void*), so this
+// struct represents the structure that HANDLE points to.
+type eventHandle struct {
+	ctx     context.Context
+	trigger func()
+}
 
 // NewMock initializes a mocked registry.
 func NewMock() *Mock {
-	return &Mock{
-		UbuntuProData: make(map[string]string),
-		OpenKeyCount:  &atomic.Int32{},
+	if !testing.Testing() {
+		panic("This registry function should be used by tests only")
 	}
+
+	// We initialize the root and Software keys, as we consider that to be the minimal
+	// "sane" Windows install.
+	m := &Mock{
+		registry: map[string]*key{filepath.Clean("/"): {
+			data: make(map[string]string),
+			mu:   &sync.RWMutex{},
+		}},
+	}
+
+	m.newKey("/Software")
+
+	m.keyHandles.data = make(map[Key]*keyHandle)
+	m.eventHandles.data = make(map[Event]*eventHandle)
+
+	return m
+}
+
+// RequireNoLeaks is a test helper to ensure we freed all allocations.
+func (r *Mock) RequireNoLeaks(t *testing.T) {
+	t.Helper()
+	require.Empty(t, r.keyHandles.data, "registry mock: leaking registry key handles")
+	require.Empty(t, r.eventHandles.data, "registry mock: leaking event handles")
 }
 
 // HKCUOpenKey mocks opening a key in the specified path under the HK_CURRENT_USER registry.
-func (r *Mock) HKCUOpenKey(path string) (uintptr, error) {
-	if path != `Software\Canonical\UbuntuPro` {
-		panic(`Attempted to access mock registry outside of HKCU\Software\Canonical\UbuntuPro`)
-	}
-
-	if r.Errors&MockErrOnOpenKey != 0 {
-		return 0, ErrMock
-	}
-
-	if !r.KeyExists {
-		return 0, ErrKeyNotExist
-	}
-
-	r.OpenKeyCount.Add(1)
-
-	// In the real implementation this integer is a pointer. Here it's just a dummy value.
-	return 1, nil
+func (r *Mock) HKCUOpenKey(path string) (Key, error) {
+	return r.openKey(path, true)
 }
 
-// CloseKey mocks releasing a key.
-func (r *Mock) CloseKey(k uintptr) {
-	r.OpenKeyCount.Add(-1)
+func (r *Mock) openKey(path string, readOnly bool) (Key, error) {
+	path = filepath.Clean("/" + path)
+
+	k, ok := r.registry[path]
+	if !ok {
+		if readOnly {
+			return 0, ErrKeyNotExist
+		}
+		k = r.newKey(path)
+		r.notify(k.parent)
+	}
+
+	return r.keyHandles.alloc(&keyHandle{
+		key:      k,
+		readOnly: readOnly,
+	}), nil
+}
+
+// CloseKey mocks releasing a key, triggering any associated events.
+func (r *Mock) CloseKey(ptr Key) {
+	defer r.keyHandles.free(ptr)
+
+	r.keyHandles.mu.Lock()
+	defer r.keyHandles.mu.Unlock()
+
+	handle, ok := r.keyHandles.data[ptr]
+
+	if !ok {
+		return
+	}
+
+	if handle.cancelCtx != nil {
+		handle.cancelCtx()
+	}
+}
+
+// CloseEvent mocks releasing an event.
+func (r *Mock) CloseEvent(ptr Event) {
+	r.eventHandles.free(ptr)
 }
 
 // ReadValue returns the value of the specified field in the specified key.
-func (r Mock) ReadValue(k uintptr, field string) (value string, err error) {
-	if k == 0 {
+func (r *Mock) ReadValue(ptr Key, field string) (value string, err error) {
+	if ptr == 0 {
 		return value, errors.New("null key")
 	}
 
-	if r.Errors&MockErrReadValue != 0 {
-		return value, ErrMock
+	r.keyHandles.mu.Lock()
+	defer r.keyHandles.mu.Unlock()
+
+	handle, ok := r.keyHandles.data[ptr]
+
+	if !ok {
+		return "", ErrKeyNotExist
 	}
 
-	v, ok := r.UbuntuProData[field]
+	return r.getValue(handle.key, field)
+}
+
+// RegNotifyChangeKeyValue creates an event and attaches it to a registry key.
+// Modifying that key or its children will trigger the event.
+// This trigger can be detected by WaitSingleObject.
+func (r *Mock) RegNotifyChangeKeyValue(ptr Key) (Event, error) {
+	r.keyHandles.mu.Lock()
+	defer r.keyHandles.mu.Unlock()
+
+	handle, ok := r.keyHandles.data[ptr]
 	if !ok {
-		return v, ErrFieldNotExist
+		return 0, ErrKeyNotExist
 	}
-	return v, nil
+
+	if handle.ctx != nil {
+		return 0, errors.New("cannot have more than one listener per key handle")
+	}
+
+	handle.ctx, handle.cancelCtx = context.WithCancel(context.Background())
+
+	// Create event
+	evHandle := r.newEvent(handle.ctx)
+
+	// Attach event to key
+	handle.key.mu.Lock()
+	defer handle.key.mu.Unlock()
+
+	handle.key.events = append(handle.key.events, evHandle)
+
+	return evHandle, nil
+}
+
+// WaitForSingleObject waits until the event is triggered. This is a blocking function.
+func (r *Mock) WaitForSingleObject(handle Event) error {
+	r.eventHandles.mu.Lock()
+	event, ok := r.eventHandles.data[handle]
+	r.eventHandles.mu.Unlock()
+
+	if !ok {
+		return errors.New("invalid event")
+	}
+
+	<-event.ctx.Done()
+	return nil
+}
+
+// HKCUOpenKeyWrite opens a key in the specified path under the HK_CURRENT_USER registry with write permissions.
+// Only tests may use it.
+func (r *Mock) HKCUOpenKeyWrite(path string) (Key, error) {
+	return r.openKey(path, false)
+}
+
+// WriteValue is used to write a value into the registry.
+func (r *Mock) WriteValue(ptr Key, field, value string) error {
+	r.keyHandles.mu.Lock()
+	defer r.keyHandles.mu.Unlock()
+
+	handle, ok := r.keyHandles.data[ptr]
+
+	if !ok {
+		return ErrKeyNotExist
+	}
+
+	if handle.readOnly {
+		return ErrAccessDenied
+	}
+
+	r.setValue(handle.key, field, value)
+
+	return nil
+}
+
+// newKey creates a key at the given path and all its parents.
+func (r *Mock) newKey(path string) *key {
+	// Clean and make abs
+	path = filepath.Clean(path)
+
+	k, ok := r.registry[path]
+	if ok {
+		// Already exists
+		return k
+	}
+
+	parent := r.newKey(filepath.Dir(path))
+
+	k = &key{
+		mu:     &sync.RWMutex{},
+		data:   make(map[string]string),
+		parent: parent,
+	}
+
+	r.registry[path] = k
+
+	return k
+}
+
+func (r *Mock) newEvent(ctx context.Context) Event {
+	ctx, cancel := context.WithCancel(ctx)
+
+	return r.eventHandles.alloc(&eventHandle{
+		ctx:     ctx,
+		trigger: cancel,
+	})
+}
+
+// Mocks memory access mapping a uintptr and real data.
+type mockedHeap[KeyType ~uintptr, DataType any] struct {
+	mu   sync.Mutex
+	data map[KeyType]DataType
+}
+
+func (h *mockedHeap[P, D]) alloc(data D) P {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// Generate a random uintptr
+	var ptr P
+	for {
+		//nolint:gosec // No need for a secure random number as this is test code
+		ptr = P(rand.Int63())
+		if ptr == 0 {
+			continue
+		}
+		if _, ok := h.data[ptr]; ok {
+			continue
+		}
+		break
+	}
+
+	h.data[ptr] = data
+	return ptr
+}
+
+func (h *mockedHeap[P, D]) free(ptr P) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	delete(h.data, ptr)
 }
