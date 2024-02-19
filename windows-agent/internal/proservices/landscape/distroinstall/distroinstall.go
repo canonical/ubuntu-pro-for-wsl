866b0fd1204fc7adf0b82d1646f26f64e657e2ce
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2024-02-19 13:32:22 +0100

    Remove now-unused user creation from distroinstall

--- windows-agent/internal/proservices/landscape/distroinstall/distroinstall.go
+++ windows-agent/internal/proservices/landscape/distroinstall/distroinstall.go
@@ -5,14 +5,9 @@ package distroinstall
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
 
 	"github.com/canonical/ubuntu-pro-for-wsl/common"
-	"github.com/ubuntu/decorate"
 	"github.com/ubuntu/gowsl"
 )
 
@@ -28,51 +23,4 @@ func InstallFromExecutable(ctx context.Context, d gowsl.Distro) error {
 	}
 
 	return err
-}
-
-// CreateUser creates a new user with the specified details in the target distro.
-func CreateUser(ctx context.Context, d gowsl.Distro, userName string, userFullName string) (uid uint32, err error) {
-	defer decorate.OnError(&err, "could not create user %q", userName)
-
-	if r, err := d.IsRegistered(); err != nil {
-		return 0, err
-	} else if !r {
-		return 0, errors.New("not registered")
-	}
-
-	if valid := UsernameIsValid(userName); !valid {
-		return 0, errors.New("username is not valid")
-	}
-
-	// strip any punctuation or any math symbols, currency signs, dingbats, box-drawing characters, etc
-	userFullName = regexp.MustCompile(`[\p{P}\p{S}]+`).ReplaceAllString(userFullName, "")
-
-	if out, err := addUserCommand(ctx, d, userName, userFullName); err != nil {
-		return 0, fmt.Errorf("could not run 'adduser': %v. Output: %s", err, out)
-	}
-
-	if out, err := addUserToGroupsCommand(ctx, d, userName); err != nil {
-		return 0, fmt.Errorf("could not add user to proper groups: %v. Output: %s", err, out)
-	}
-
-	if out, err := removePasswordCommand(ctx, d, userName); err != nil {
-		return 0, fmt.Errorf("could not enable login: %v. Output: %s", err, out)
-	}
-
-	out, err := getUserIDCommand(ctx, d, userName)
-	if err != nil {
-		return 0, fmt.Errorf("user id could not be retreived: %v. Output: %s", err, out)
-	}
-
-	id64, err := strconv.ParseUint(strings.TrimSpace(string(out)), 10, 32)
-	if err != nil {
-		return 0, fmt.Errorf("could not parse uid %q: %v", string(out), err)
-	}
-
-	return uint32(id64), nil
-}
-
-// UsernameIsValid returns true if the username matches the WSL regex for usernames.
-func UsernameIsValid(userName string) bool {
-	return regexp.MustCompile(`^[a-z][-a-z0-9_]*$`).MatchString(userName)
 }
