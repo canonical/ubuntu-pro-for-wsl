d6a91f276aa61d028508f7d15059ce0f549ce7a1
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2024-02-29 19:00:44 +0100

    Linter warnings

--- windows-agent/internal/proservices/registrywatcher/registry/registry_linux.go
+++ windows-agent/internal/proservices/registrywatcher/registry/registry_linux.go
@@ -8,7 +8,7 @@ func (Windows) HKCUOpenKey(path string) (Key, error) {
 	panic("the Windows registry is not available on Linux")
 }
 
-// HKCUOpenKey creates a key in the specified path under the HK_CURRENT_USER registry with write permissions.
+// HKCUCreateKey creates a key in the specified path under the HK_CURRENT_USER registry with write permissions.
 func (Windows) HKCUCreateKey(path string) (Key, error) {
 	panic("the Windows registry is not available on Linux")
 }
@@ -23,6 +23,7 @@ func (Windows) ReadValue(k Key, field string) (string, error) {
 	panic("the Windows registry is not available on Linux")
 }
 
+// WriteValue writes the value to the specified field in the specified key.
 func (Windows) WriteValue(k Key, field, value string, multiLine bool) error {
 	panic("the Windows registry is not available on Linux")
 }
