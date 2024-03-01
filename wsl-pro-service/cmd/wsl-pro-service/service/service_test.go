40b2e1a4c7cfa779de7f0a9fd954dfe03f4960da
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2024-03-01 18:12:36 +0100

    Fix test in deb package

--- wsl-pro-service/cmd/wsl-pro-service/service/service_test.go
+++ wsl-pro-service/cmd/wsl-pro-service/service/service_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/canonical/ubuntu-pro-for-wsl/common"
 	"github.com/canonical/ubuntu-pro-for-wsl/wsl-pro-service/cmd/wsl-pro-service/service"
 	"github.com/canonical/ubuntu-pro-for-wsl/wsl-pro-service/internal/system"
 	"github.com/canonical/ubuntu-pro-for-wsl/wsl-pro-service/internal/testutils"
@@ -66,7 +67,7 @@ func TestVersion(t *testing.T) {
 	}
 
 	require.Equal(t, want, fields[0], "Wrong executable name")
-	require.Equal(t, "Dev", fields[1], "Wrong version")
+	require.Equal(t, common.Version, fields[1], "Wrong version")
 }
 
 func TestNoUsageError(t *testing.T) {
