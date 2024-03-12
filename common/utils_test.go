9654f5e1526e2a68621ae11be2a2fb504e4b3d2e
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2024-03-12 12:43:18 +0100

    Fuzz common.Obfuscate

--- common/utils_test.go
+++ common/utils_test.go
@@ -1,6 +1,7 @@
 package common_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/canonical/ubuntu-pro-for-wsl/common"
@@ -38,4 +39,45 @@ func TestWSLLauncher(t *testing.T) {
 			require.Equal(t, tc.want, got, "Unexpected return value for WSLLauncher")
 		})
 	}
+}
+
+func FuzzObfuscate(f *testing.F) {
+	f.Add("Hello, World!")
+	f.Add("")
+	f.Add("123")
+	f.Add("12a345lgsdlfasd67890")
+
+	f.Fuzz(func(t *testing.T, input string) {
+		got := common.Obfuscate(input)
+
+		t.Log("Input: ", input)
+		t.Log("Output: ", got)
+
+		switch len(input) {
+		case 0:
+			require.Equal(t, "", got, "Obfuscate should return an empty string when the input is empty")
+			return
+		case 1:
+			require.Equal(t, "*", got, "Obfuscate should reveal no characters when the input is a single character")
+			return
+		case 2:
+			require.Equal(t, "**", got, "Obfuscate should reveal no characters when the input is two characters")
+			return
+		case 3:
+			require.Equal(t, "***", got, "Obfuscate should reveal no characters when the input is three characters")
+			return
+		case 4:
+			require.Equal(t, "****", got, "Obfuscate should reveal no characters when the input is four characters")
+			return
+		}
+
+		endPrefix := 2
+		beginSuffix := len(input) - 2
+
+		require.Equal(t, input[:endPrefix], got[:endPrefix], "Obfuscate should reveal the first two characters")
+		require.Equal(t, input[beginSuffix:], got[beginSuffix:], "Obfuscate should reveal the last two characters")
+
+		nAsterisks := strings.Count(got[endPrefix:beginSuffix], "*")
+		require.Equal(t, beginSuffix-endPrefix, nAsterisks, "Obfuscate should not reveal any characters in the middle")
+	})
 }
