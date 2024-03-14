b612323d97d7e095a0b292fd6cf1e93e3bec1403
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2024-03-14 13:05:47 +0100

    Remove wslserviceapi

--- wslserviceapi/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-
-PATH=$PATH:$(go env GOPATH)/bin protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative wslserviceapi.proto
