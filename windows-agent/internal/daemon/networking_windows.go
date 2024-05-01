808551135c4ad714783f740fc14c081daa97d459
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-05-01 01:08:19 -0300

    This is a combination of 2 commits.
    Fills the blanks with Windows specific behavior
    
    A typedef to replace the windows.ipAdapterAddresses struct as that's not
    available on Linux.
    That allows the Linux version to implement a suitable replacement.
    Very shallow wrapping around the low level data structure to expose
    a nicer API returning strings and net.IP.
    fillFromTemplate is a helper for the mocks to be able to construct the
    right type during tests.

--- windows-agent/internal/daemon/networking_windows.go
+++ windows-agent/internal/daemon/networking_windows.go
@@ -1,144 +1,85 @@
 package daemon
 
 import (
-	"context"
-	"errors"
 	"net"
-	"os/exec"
-	"reflect"
+	"syscall"
+	"unsafe"
 
-	"github.com/ubuntu/decorate"
+	"github.com/canonical/ubuntu-pro-for-wsl/common/testdetection"
 	"golang.org/x/sys/windows"
 )
 
-type winIPAdapterAddresses struct {
-	node *windows.IpAdapterAddresses
+func init() {
+	defaultOptions = options{
+		wslSystemCmd:         []string{"wsl.exe", "--system", "wslinfo", "--networking-mode", "-n"},
+		getAdaptersAddresses: getWindowsAdaptersAddresses,
+	}
 }
 
-func (n *winIPAdapterAddresses) Next() ipAdapterAddresses {
-	if n.node.Next == nil {
+// getWindowsAdaptersAddresses is a wrapper around windows.GetAdaptersAddresses that accepts our custom ipAdapterAddresses type so we can mock it in tests.
+//
+//nolint:unused // When linting with gowslmock built tag, this is flagged as unused because it's replaced with the mocked version. The rest of the file is still used.
+func getWindowsAdaptersAddresses(family uint32, flags uint32, reserved uintptr, adapterAddresses *ipAdapterAddresses, sizePointer *uint32) (errcode error) {
+	return windows.GetAdaptersAddresses(family, flags, reserved, (*windows.IpAdapterAddresses)(adapterAddresses), sizePointer)
+}
+
+// ipAdapterAddresses is a wrapper around windows.IpAdapterAddresses to provide handy accessors and allow replacing with something else on Linux for testing.
+type ipAdapterAddresses windows.IpAdapterAddresses
+
+func (a *ipAdapterAddresses) next() *ipAdapterAddresses {
+	return (*ipAdapterAddresses)(a.Next)
+}
+
+func (a *ipAdapterAddresses) friendlyName() string {
+	return windows.UTF16PtrToString(a.FriendlyName)
+}
+
+func (a *ipAdapterAddresses) description() string {
+	return windows.UTF16PtrToString(a.Description)
+}
+
+func (a *ipAdapterAddresses) ip() net.IP {
+	return a.FirstUnicastAddress.Address.IP()
+}
+
+// ipToRawSockaddrAny is a helper function that converts an assumed valid net.IP (IPv4) to *syscall.RawSockaddrAny.
+func ipToRawSockaddrAny(ip net.IP) *syscall.RawSockaddrAny {
+	testdetection.MustBeTesting()
+
+	ip4 := ip.To4()
+	if ip4 == nil {
 		return nil
 	}
-	return &winIPAdapterAddresses{n.node.Next}
+
+	sa := new(syscall.RawSockaddrInet4)
+	sa.Family = syscall.AF_INET
+	copy(sa.Addr[:], ip4) // ip4 is already a 4-byte slice
+
+	//nolint:gosec // Unsafe is required to manipulate pointers at the Win32 API level, only used in tests.
+	return (*syscall.RawSockaddrAny)(unsafe.Pointer(sa))
 }
 
-func (n *winIPAdapterAddresses) Description() string {
-	return windows.UTF16PtrToString(n.node.Description)
-}
+// fillFromTemplate fills the ipAdapterAddresses struct with the values from the mockIPAddrsTemplate struct for testing purposes.
+func (a *ipAdapterAddresses) fillFromTemplate(template *mockIPAddrsTemplate, next *ipAdapterAddresses) {
+	testdetection.MustBeTesting()
 
-func (n *winIPAdapterAddresses) FriendlyName() string {
-	return windows.UTF16PtrToString(n.node.FriendlyName)
-}
+	// To constrain complexity we only fill the pieces of data we need in the actual code, skipping a lot of details and fields.
+	a.FriendlyName = windows.StringToUTF16Ptr(template.friendlyName)
+	a.Description = windows.StringToUTF16Ptr(template.desc)
 
-func (n *winIPAdapterAddresses) IP() net.IP {
-	return n.node.FirstUnicastAddress.Address.IP()
-}
+	ip := ipToRawSockaddrAny(template.ip)
 
-// getAdaptersAddresses returns the head of a linked list of network adapters.
-func (i ipConfig) getAdaptersAddresses() (head ipAdapterAddresses, err error) {
-	defer decorate.OnError(&err, "could not get network adapter addresses")
-
-	// This function is a wrapper around the Windows API GetAdaptersAddresses.
-	// https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getadaptersaddresses
-	//
-	// This function takes in a buffer and fills it with a linked list.
-
-	// Flags from the Windows API.
-	//nolint:revive // Windows API constants are in shout case.
-	const (
-		GAA_FLAG_SKIP_ANYCAST    uint32 = 0x0002
-		GAA_FLAG_SKIP_MULTICAST  uint32 = 0x0004
-		GAA_FLAG_SKIP_DNS_SERVER uint32 = 0x0008
-	)
-
-	// Return only IPv4 unicast addresses
-	const (
-		family uint32 = windows.AF_INET
-		flags  uint32 = GAA_FLAG_SKIP_ANYCAST | GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER
-	)
-
-	// We need a typed buffer rather than []byte because we don't want the GC to move
-	// the buffer around while we're using it, invalidating the NEXT pointers.
-	var buff buffer[windows.IpAdapterAddresses]
-
-	// Win32 API docs recommend a buff size of 15KB.
-	buff.resizeBytes(15 * kilobyte)
-
-	for range 10 {
-		size := buff.byteCount()
-		err := windows.GetAdaptersAddresses(family, flags, 0, &buff.data[0], &size)
-		if errors.Is(err, windows.ERROR_BUFFER_OVERFLOW) {
-			// Buffer too small, try again with the returned size.
-			buff.resizeBytes(size)
-			continue
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		// The buffer is filled with the linked list of adapters, with the first element being the head.
-		// We return a pointer to the start of the buffer.
-		return &winIPAdapterAddresses{buff.ptr()}, nil
+	a.FirstUnicastAddress = &windows.IpAdapterUnicastAddress{
+		Address: windows.SocketAddress{
+			Sockaddr:       ip,
+			SockaddrLength: int32(unsafe.Sizeof(windows.RawSockaddrInet4{})),
+		},
 	}
 
-	// We tried 10 times and the buffer is still too small: give up.
-	return nil, errors.New("iteration limit reached")
+	a.Next = (*windows.IpAdapterAddresses)(next)
 }
 
-type ipConfig struct{}
-
-func newIPConfig() ipConfig {
-	return ipConfig{}
-}
-
-// Constants for byte size conversion.
-const kilobyte uint32 = 1024
-
-// buffer is a type that allows resizing a slice of any type to a given number of bytes.
-type buffer[T any] struct {
-	data []T
-}
-
-// byteCount returns the number of bytes in the buffer.
-func (b buffer[T]) byteCount() uint32 {
-	var t T
-	sizeOf := uint32(reflect.TypeOf(t).Size())
-	n := uint32(len(b.data))
-	return n * sizeOf
-}
-
-// ResizeBytes resizes the buffer to the given number of bytes, rounded UP to fit an integer element size.
-func (b *buffer[T]) resizeBytes(n uint32) {
-	var t T
-	sizeOf := uint32(reflect.TypeOf(t).Size())
-
-	newLen := int(n / sizeOf)
-	if n%sizeOf != 0 {
-		newLen++
-	}
-
-	if newLen > len(b.data) {
-		b.data = make([]T, newLen)
-	}
-}
-
-// ptr returns a pointer to the start of the buffer.
-func (b *buffer[T]) ptr() *T {
-	if len(b.data) == 0 {
-		return nil
-	}
-	return &b.data[0]
-}
-
-type wslSystem struct{}
-
-func newWslSystem() wslSystem {
-	return wslSystem{}
-}
-
-// Command provides an *exec.Command configured to run inside the always present system distro, useful when we cannot guaratee the presence of any other distro instance.
-func (wsl wslSystem) Command(ctx context.Context, name string, arg ...string) *exec.Cmd {
-	args := append([]string{"--system", name}, arg...)
-	//nolint: gosec // Subprocess launched with variable (gosec) intentionally so we can mock it.
-	return exec.CommandContext(ctx, "wsl", args...)
-}
+// ERROR_BUFFER_OVERFLOW is defined as a constant here so we can redefine it in tests on Linux.
+//
+//nolint:revive // Windows API constants are in shout case.
+const ERROR_BUFFER_OVERFLOW = windows.ERROR_BUFFER_OVERFLOW
