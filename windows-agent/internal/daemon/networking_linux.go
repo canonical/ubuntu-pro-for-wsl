ce2cee116e8b4f02dffbe853edca0f3260e458d5
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-05-08 10:23:28 -0300

    Makes the Linux impl of GetAdapterAddresses defer to testutils
    The Linux impl is just a thin wrapper exposing the accessors.

--- windows-agent/internal/daemon/networking_linux.go
+++ windows-agent/internal/daemon/networking_linux.go
@@ -2,26 +2,20 @@ package daemon
 
 import (
 	"net"
-	"syscall"
 
-	"github.com/canonical/ubuntu-pro-for-wsl/common/testdetection"
+	"github.com/canonical/ubuntu-pro-for-wsl/windows-agent/internal/daemon/testutils"
 )
 
 // ERROR_BUFFER_OVERFLOW is the error returned by GetAdaptersAddresses when the buffer is too small.
 //
 //nolint:revive // Windows API constants are in shout case.
-const ERROR_BUFFER_OVERFLOW = syscall.EOVERFLOW
+const ERROR_BUFFER_OVERFLOW = testutils.ERROR_BUFFER_OVERFLOW
 
 // ipAdapterAddresses redefines the wrapper type for the IP_ADAPTER_ADDRESSES structure for testing on Linux.
-type ipAdapterAddresses struct {
-	Next                *ipAdapterAddresses
-	FriendlyName        string
-	Description         string
-	FirstUnicastAddress net.IP
-}
+type ipAdapterAddresses testutils.IPAdapterAddresses
 
 func (a *ipAdapterAddresses) next() *ipAdapterAddresses {
-	return a.Next
+	return (*ipAdapterAddresses)(a.Next)
 }
 
 func (a *ipAdapterAddresses) friendlyName() string {
@@ -36,13 +30,7 @@ func (a *ipAdapterAddresses) ip() net.IP {
 	return a.FirstUnicastAddress
 }
 
-// fillFromTemplate fills the ipAdapterAddresses structure with the values from the template.
-func (a *ipAdapterAddresses) fillFromTemplate(template *mockIPAddrsTemplate, next *ipAdapterAddresses) {
-	testdetection.MustBeTesting()
-
-	a.FriendlyName = template.friendlyName
-	a.Description = template.desc
-
-	a.FirstUnicastAddress = template.ip
-	a.Next = next
+// getWindowsAdaptersAddresses is a fake wrapper that panics if invoked, which only exists to satisfy setting `defaultOptions` in networking.go with a Linux "implementation".
+func getWindowsAdaptersAddresses(family, flags uint32, reserved uintptr, adapterAddresses *ipAdapterAddresses, sizePointer *uint32) (errcode error) {
+	panic("getWindowsAdaptersAddresses is not implemented on Linux without a mock")
 }
