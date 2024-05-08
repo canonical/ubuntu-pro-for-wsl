5e1e50c28b24a61f5b14c7f271eead73dba8cab3
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-05-08 10:49:17 -0300

    Uses init for integration tests only to set default mock options.
    That enables running integration tests even where there is no
    WSL adapter nor wsl.exe
    Otherwise the Flutter integration tests won't have a way
    to signal to the agent that the mocks must be used.

---
+++ windows-agent/internal/daemon/integration_testing.go
@@ -0,0 +1,13 @@
+//go:build integrationtests
+
+package daemon
+
+import (
+	"github.com/canonical/ubuntu-pro-for-wsl/common/testdetection"
+	"github.com/canonical/ubuntu-pro-for-wsl/windows-agent/internal/daemon/testutils"
+)
+
+func init() {
+	testdetection.MustBeTesting()
+	testutils.SetDefaultMockOptions()
+}
