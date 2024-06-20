abef0d19dad5be289bb6d575eb1801054be6d5e7
Didier Roche <didier.roche@canonical.com>
2024-06-20 10:57:46 +0200

    Only override computer_title and ssl in landscape gRPC calls
    The other functionalities are now moved to the windows agent, as they
    are common between every wsl instances.
    Reformat the code to allow hooking up a new API for calling
    LandscapeEnable during system init.
    
    We allow changing the configuration file afterwards and detect any
    changes from the given one.

--- wsl-pro-service/internal/system/landscape.go
+++ wsl-pro-service/internal/system/landscape.go
@@ -19,23 +19,7 @@ const (
 
 // LandscapeEnable registers the current distro to Landscape with the specified config.
 func (s *System) LandscapeEnable(ctx context.Context, landscapeConfig string, hostagentUID string) (err error) {
-	// Decorating here to avoid stuttering the URL (url package prints it as well)
-	defer decorate.OnError(&err, "could not register distro to Landscape")
-
-	if landscapeConfig, err = modifyConfig(ctx, s, landscapeConfig, hostagentUID); err != nil {
-		return err
-	}
-
-	if err := s.writeConfig(landscapeConfig); err != nil {
-		return err
-	}
-
-	cmd := s.backend.LandscapeConfigExecutable(ctx, "--config", landscapeConfigPath, "--silent")
-	if _, err := runCommand(cmd); err != nil {
-		return fmt.Errorf("could not enable Landscape: %v", err)
-	}
-
-	return nil
+	return s.fixAndEnableLandscapeFromConfig(ctx, landscapeConfig, true)
 }
 
 // LandscapeDisable unregisters the current distro from Landscape.
@@ -43,6 +27,40 @@ func (s *System) LandscapeDisable(ctx context.Context) (err error) {
 	cmd := s.backend.LandscapeConfigExecutable(ctx, "--disable")
 	if _, err := runCommand(cmd); err != nil {
 		return fmt.Errorf("could not disable Landscape:%v", err)
+	}
+
+	return nil
+}
+
+func (s *System) fixAndEnableLandscapeFromConfig(ctx context.Context, landscapeConfig string, enableUnconditionally bool) (err error) {
+	// Decorating here to avoid stuttering the URL (url package prints it as well)
+	defer decorate.OnError(&err, "could not register distro to Landscape")
+
+	r := strings.NewReader(landscapeConfig)
+	iniFile, err := ini.Load(r)
+	if err != nil {
+		return fmt.Errorf("could not parse config: %v", err)
+	}
+
+	modifiedLandscapeConfig, err := normalizeLandscapeConfig(ctx, s, iniFile)
+	if err != nil {
+		return err
+	}
+
+	// No change to do, do not rewrite config
+	if !enableUnconditionally && modifiedLandscapeConfig == landscapeConfig {
+		log.Debug(ctx, "Landscape configuration is already valid")
+		return nil
+	}
+
+	if err := s.writeConfig(modifiedLandscapeConfig); err != nil {
+		return err
+	}
+
+	// TODO: check foreground/background
+	cmd := s.backend.LandscapeConfigExecutable(ctx, "--config", landscapeConfigPath, "--silent")
+	if _, err := runCommand(cmd); err != nil {
+		return fmt.Errorf("could not enable Landscape: %v", err)
 	}
 
 	return nil
@@ -86,89 +104,50 @@ func (s *System) writeConfig(landscapeConfig string) (err error) {
 	return nil
 }
 
-// modifyConfig overrides parameters in the configuration to adapt them to the current distro.
-func modifyConfig(ctx context.Context, s *System, landscapeConfig string, hostagentUID string) (string, error) {
-	if landscapeConfig == "" {
-		return "", nil
-	}
-
-	r := strings.NewReader(landscapeConfig)
-	data, err := ini.Load(r)
+// normalizeLandscapeConfig ensures that the landscape config has the expected computer_title and SSL certificate path
+// transformed in a Linux path.
+func normalizeLandscapeConfig(ctx context.Context, s *System, iniFile *ini.File) (modifiedLandscapeConfig string, err error) {
+	clientSection, err := iniFile.GetSection("client")
 	if err != nil {
-		return "", fmt.Errorf("could not parse config: %v", err)
+		return "", err
 	}
 
-	data.DeleteSection("host")
-
+	// Add or refresh computer title
 	distroName, err := s.WslDistroName(ctx)
 	if err != nil {
 		return "", err
 	}
-	if err := createKey(ctx, data, "client", "computer_title", distroName, true); err != nil {
-		return "", err
+	oldComputerTitle, err := clientSection.GetKey("computer_title")
+	if err != nil {
+		if _, err = clientSection.NewKey("computer_title", distroName); err != nil {
+			return "", err
+		}
+	} else if oldComputerTitle.String() != distroName {
+		oldComputerTitle.SetValue(distroName)
 	}
 
-	if err := createKey(ctx, data, "client", "hostagent_uid", hostagentUID, true); err != nil {
-		return "", err
-	}
-
-	if err := createKey(ctx, data, "client", "tags", "wsl", false); err != nil {
-		return "", err
-	}
-
-	if err := overrideSSLCertificate(ctx, s, data); err != nil {
+	// Refresh SSL certificate path if any
+	if err := overrideSSLCertificate(ctx, s, clientSection); err != nil {
 		return "", fmt.Errorf("could not override SSL certificate path: %v", err)
 	}
 
+	// Return the modified config as a string.
 	w := &bytes.Buffer{}
-	if _, err := data.WriteTo(w); err != nil {
-		return "", fmt.Errorf("could not write modified config: %v", err)
+	if _, err := iniFile.WriteTo(w); err != nil {
+		return "", fmt.Errorf("could not regenerate modified config: %v", err)
 	}
 
 	return w.String(), nil
 }
 
-// createKey tries to create a key with a particular value, optionally overriding an existing key.
-func createKey(ctx context.Context, data *ini.File, section, key, value string, override bool) error {
-	sec, err := data.GetSection(section)
-	if err != nil {
-		if sec, err = data.NewSection(section); err != nil {
-			return fmt.Errorf("could not find nor create section %q: %v", section, err)
-		}
-	}
-
-	if sec.HasKey(key) {
-		if !override {
-			log.Infof(ctx, "Landscape config contains key %q. Its value will not be overridden.", key)
-			return nil
-		}
-
-		log.Infof(ctx, "Landscape config contains key %q. Its value will be overridden.", key)
-		sec.DeleteKey(key)
-	}
-
-	if _, err := sec.NewKey(key, value); err != nil {
-		return fmt.Errorf("could not create key %q: %v", key, err)
-	}
-
-	return nil
-}
-
 // overrideComputerTitle converts the ssl_public_key field in the Landscape config
 // from a Windows path to a Linux path.
-func overrideSSLCertificate(ctx context.Context, s *System, data *ini.File) error {
-	const section = "client"
+func overrideSSLCertificate(ctx context.Context, s *System, section *ini.Section) error {
 	const key = "ssl_public_key"
 
-	sec, err := data.GetSection(section)
+	k, err := section.GetKey(key)
 	if err != nil {
-		// No certificate
-		return nil
-	}
-
-	k, err := sec.GetKey(key)
-	if err != nil {
-		// No certificate
+		// No certificate: nothing to transform
 		return nil
 	}
 
@@ -182,6 +161,5 @@ func overrideSSLCertificate(ctx context.Context, s *System, data *ini.File) erro
 
 	pathLinux := s.Path(strings.TrimSpace(string(out)))
 	k.SetValue(pathLinux)
-
 	return nil
 }
