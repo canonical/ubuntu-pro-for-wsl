ee4a669f03177401d5bfd8c0e6ff74f378f73d34
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-08-27 17:00:12 -0300

    Clearer comments in checkError
    All about the semantics of calling a dll Proc.

--- storeapi/go-wrapper/microsoftstore/store.go
+++ storeapi/go-wrapper/microsoftstore/store.go
@@ -34,20 +34,29 @@ func checkError(hres int64, err error) (int64, error) {
 	// From syscall/dll_windows.go (*Proc).Call doc:
 	// > Callers must inspect the primary return value to decide whether an
 	//   error occurred [...] before consulting the error.
+	// There is no possibility of nil  error, the `err` return value is always constructed with the
+	// result of `GetLastError()` which could have been set by something completely
+	// unrelated to our code some time in the past, as well as it could be `ERROR_SUCCESS` which is the `Errno(0)`.
+	// If the act of calling the API fails (not the function we're calling, but the attempt to call it), then we'd
+	// have a meaningful `syscall.Errno` object via the `err` parameter, related to the actual failure (like a function not found in this DLL)
+	// Since our implementation of the store API doesn't touch errno the call should return `hres`
+	// in our predefined range plus garbage in the `err` argument, thus we only care about the `hres` in this case.
 	if e := NewStoreAPIError(hres); e != nil {
 		return hres, fmt.Errorf("storeApi returned error code %d: %w", hres, e)
 	}
 
+	// Supposedly unreachable: proc.Call must always return a non-nil syscall.Errno
 	if err == nil {
 		return hres, nil
 	}
 
 	var target syscall.Errno
 	if b := errors.As(err, &target); !b {
-		// Supposedly unreachable: proc.Call must always return a syscall.Errno
+		// Supposedly unreachable: proc.Call must always return a non-nil syscall.Errno
 		return hres, err
 	}
 
+	// The act of calling our API didn't succeed, function not found in the DLL for example:
 	if target != syscall.Errno(0) {
 		return hres, fmt.Errorf("failed syscall to storeApi: %v (syscall errno %d)", target, err)
 	}
