7b572c17784fb148a346fe1f20882f31921afdf1
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-09-02 18:06:04 -0300

    Adds a test defining the semantics of daemon.Restart()
    - Should not fail if called multiple times
    - Should not fail if called after Quit
    - Should not fail if called before Serve
    - Should block at least until the gRPC server is stopped.

--- windows-agent/internal/daemon/daemon_test.go
+++ windows-agent/internal/daemon/daemon_test.go
@@ -158,6 +158,113 @@ func TestStartQuit(t *testing.T) {
 	}
 }
 
+func TestRestart(t *testing.T) {
+	t.Parallel()
+
+	testsCases := map[string]struct {
+		afterQuit     bool
+		beforeServing bool
+		cancelEarly   bool
+
+		wantAddrFileDeleted bool
+		wantServeErr        bool
+	}{
+		"Success": {},
+		"Does nothing when the context is cancelled":  {cancelEarly: true, wantAddrFileDeleted: true, wantServeErr: true},
+		"Does nothing when daemon is not serving yet": {beforeServing: true},
+		"Does nothing when the daemon is done":        {afterQuit: true, wantAddrFileDeleted: true},
+	}
+
+	for name, tc := range testsCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			addrDir := t.TempDir()
+
+			registerer := func(context.Context, bool) *grpc.Server {
+				server := grpc.NewServer()
+				var service testGRPCService
+				grpctestservice.RegisterTestServiceServer(server, service)
+				return server
+			}
+
+			d := daemon.New(ctx, registerer, addrDir)
+
+			serveErr := make(chan error, 1)
+
+			if tc.beforeServing {
+				go func() {
+					d.Restart(ctx)
+					serveErr <- nil
+				}()
+
+				select {
+				case <-time.After(100 * time.Millisecond):
+					require.Fail(t, "Restart should return immediately when daemon is not serving")
+				case <-serveErr:
+					// proceed.
+				}
+			}
+
+			go func() {
+				serveErr <- d.Serve(ctx)
+				close(serveErr)
+			}()
+
+			addrPath := filepath.Join(addrDir, common.ListeningPortFileName)
+
+			var err error
+			requireWaitPathExists(t, addrPath, "Serve should have created a .address file")
+			addrSt, err := os.Stat(addrPath)
+			require.NoError(t, err, "Address file should be readable")
+
+			if tc.afterQuit {
+				d.Quit(ctx, false)
+			}
+			if tc.cancelEarly {
+				cancel()
+			}
+			// Now we know the GRPC server has started serving.
+			d.Restart(ctx)
+
+			// d.Serve() shouldn't have exitted with an error yet at this point.
+			select {
+			case err := <-serveErr:
+				if tc.wantServeErr {
+					require.Error(t, err, "Serve should return with error when stopped by the context")
+				} else {
+					require.NoError(t, err, "Restart should not have caused Serve() to exit with an error")
+				}
+			case <-time.After(100 * time.Millisecond):
+				// proceed.
+			}
+
+			if tc.wantAddrFileDeleted {
+				requireWaitPathDoesNotExist(t, addrPath, "Address file should have been removed after quitting the server")
+				return
+			}
+
+			requireWaitPathExists(t, addrPath, "Restart should have caused creation of another .address file")
+			// Contents could be the same without our control, thus best to check the file time.
+			newAddrSt, err := os.Stat(addrPath)
+			require.NoError(t, err, "Address file should be readable")
+			require.NotEqual(t, addrSt.ModTime(), newAddrSt.ModTime(), "Address file should be overwritten after Restart")
+
+			// Restart a second time
+			d.Restart(ctx)
+			// d.Serve() shouldn't have exitted with an error yet at this point.
+			select {
+			case err := <-serveErr:
+				require.NoError(t, err, "Restart should not have caused Serve() to exit with an error")
+			case <-time.After(100 * time.Millisecond):
+				// proceed.
+			}
+		})
+	}
+}
+
 func TestServeWSLIP(t *testing.T) {
 	t.Parallel()
 
