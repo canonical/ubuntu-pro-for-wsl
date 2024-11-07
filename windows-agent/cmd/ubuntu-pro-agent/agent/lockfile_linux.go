7344beb70d26f535cc7dba47b9cb60c45cd79918
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-11-07 12:34:19 -0300

    Use decorate in the lockfile impl

--- windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_linux.go
+++ windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_linux.go
@@ -5,19 +5,23 @@ import (
 	"fmt"
 	"os"
 	"syscall"
+
+	"github.com/ubuntu/decorate"
 )
 
 // createLockFile tries to create or open an empty file with given name with exclusive access.
 // If the file already exists AND is still locked, it will fail.
-func createLockFile(path string) (*os.File, error) {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0600)
+func createLockFile(path string) (f *os.File, err error) {
+	decorate.OnError(&err, "could not create lock file %s: %v", path, err)
+
+	f, err = os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0600)
 	if err != nil {
-		return nil, fmt.Errorf("could not create lock file %s: %v", path, err)
+		return nil, err
 	}
 	// This would only fail if the file is locked by another process.
 	err = syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
 	if err != nil {
-		return nil, fmt.Errorf("could not lock file %s: %v", path, errors.Join(err, f.Close()))
+		return nil, fmt.Errorf("could not lock file: %v", errors.Join(err, f.Close()))
 	}
 
 	return f, nil
--- windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_windows.go
+++ windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_windows.go
@@ -1,21 +1,19 @@
 package agent
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/ubuntu/decorate"
 )
 
 // createLockFile tries to create or open an empty file with given name with exclusive access.
-func createLockFile(path string) (*os.File, error) {
+func createLockFile(path string) (f *os.File, err error) {
+	decorate.OnError(&err, "could not create lock file %s: %v", path, err)
+
 	// On Windows removing fails if the file is opened by another process with ERROR_SHARING_VIOLATION.
 	if err := os.Remove(path); err != nil && !os.IsNotExist(err) {
-		return nil, fmt.Errorf("could not remove lock file %s: %v", path, err)
+		return nil, err
 	}
 	// If this process is the only instance of this program, then the file won't exist.
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_EXCL, 0600)
-	if err != nil {
-		return nil, fmt.Errorf("could not create lock file %s: %v", path, err)
-	}
-
-	return f, nil
+	return os.OpenFile(path, os.O_CREATE|os.O_EXCL, 0600)
 }
