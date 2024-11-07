fab082b28c13148f2e0427c82fc6f8b2795c00b5
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-11-07 10:15:54 -0300

    Rename local file variable as 'f'
    Keep consistency with existing practices.

--- windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_linux.go
+++ windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_linux.go
@@ -10,15 +10,15 @@ import (
 // createLockFile tries to create or open an empty file with given name with exclusive access.
 // If the file already exists AND is still locked, it will fail.
 func createLockFile(path string) (*os.File, error) {
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0600)
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0600)
 	if err != nil {
 		return nil, fmt.Errorf("could not create lock file %s: %v", path, err)
 	}
 	// This would only fail if the file is locked by another process.
-	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	err = syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
 	if err != nil {
-		return nil, fmt.Errorf("could not lock file %s: %v", path, errors.Join(err, file.Close()))
+		return nil, fmt.Errorf("could not lock file %s: %v", path, errors.Join(err, f.Close()))
 	}
 
-	return file, nil
+	return f, nil
 }
--- windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_windows.go
+++ windows-agent/cmd/ubuntu-pro-agent/agent/lockfile_windows.go
@@ -12,10 +12,10 @@ func createLockFile(path string) (*os.File, error) {
 		return nil, fmt.Errorf("could not remove lock file %s: %v", path, err)
 	}
 	// If this process is the only instance of this program, then the file won't exist.
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_EXCL, 0600)
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
 		return nil, fmt.Errorf("could not create lock file %s: %v", path, err)
 	}
 
-	return file, nil
+	return f, nil
 }
