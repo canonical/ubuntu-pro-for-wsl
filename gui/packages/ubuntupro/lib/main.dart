925c035035913ae203d66567eeeac28dfec6a89a
ashuntu <ashton.nelson@canonical.com>
2025-01-06 16:00:53 -0600

    Set a minimum size for the window

--- gui/packages/ubuntupro/lib/main.dart
+++ gui/packages/ubuntupro/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:windows_single_instance/windows_single_instance.dart';
 import 'package:yaru/widgets.dart';
+
 import 'app.dart';
 import 'constants.dart';
 import 'core/agent_api_client.dart';
@@ -16,6 +18,9 @@ Future<void> main() async {
     [],
     'UP4W_SINGLE_INSTANCE_GUI',
   );
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
   final agentMonitor = AgentStartupMonitor(
     addrFileName: kAddrFileName,
     agentLauncher: launch,
@@ -26,6 +31,16 @@ Future<void> main() async {
   final settings = Environment()['UP4W_INTEGRATION_TESTING'] != null
       ? Settings.withOptions(Options.withAll)
       : Settings(SettingsRepository());
+
+  final windowOptions = const WindowOptions(
+    size: Size(900, 550),
+    minimumSize: Size(900, 550),
+    center: true,
+  );
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
 
   runApp(Pro4WSLApp(agentMonitor, settings));
 }
