93c82af97d33d5420cc6659b8dff5b8b7c12dc37
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2025-01-30 11:08:26 -0300

    Test when launching 'sleep 5' fails.
    For real, it would mean a broken image or vhdx.
    For tests we can leverage some toggles offered by GoWSL mocks.

--- windows-agent/internal/proservices/landscape/executor_test.go
+++ windows-agent/internal/proservices/landscape/executor_test.go
@@ -158,6 +158,7 @@ func TestInstall(t *testing.T) {
 		distroAlreadyInstalled bool
 		distroName             string
 		wslInstallErr          bool
+		wslLaunchErr           bool
 		appxDoesNotExist       bool
 		nonResponsiveServer    bool
 		breakVhdxDir           bool
@@ -183,6 +184,7 @@ func TestInstall(t *testing.T) {
 		"Error when the distro fails to install":     {wslInstallErr: true},
 		"Error when cannot write cloud-init file":    {cloudInitWriteErr: true, wantCloudInitWriteCalled: true},
 
+		"Error when launching the new distro fails":                       {wslLaunchErr: true, sendRootfsURL: "goodfile", wantInstalled: false},
 		"Error when the distro ID is reserved (Ubuntu)":                   {sendRootfsURL: "goodfile", distroName: "Ubuntu", wantInstalled: false},
 		"Error when the distro ID is reserved (Preview)":                  {sendRootfsURL: "goodfile", distroName: "Ubuntu-Preview", wantInstalled: false},
 		"Error when the distro ID is reserved (case sensitiveness)":       {sendRootfsURL: "goodfile", distroName: "ubuntu-preview", wantInstalled: false},
@@ -267,6 +269,11 @@ func TestInstall(t *testing.T) {
 
 					if tc.wslInstallErr {
 						testBed.wslMock.InstallError = true
+					}
+
+					if tc.wslLaunchErr {
+						testBed.wslMock.WslLaunchInteractiveError = true
+						testBed.wslMock.WslLaunchError = true
 					}
 
 					if tc.cloudInitExecFailure {
