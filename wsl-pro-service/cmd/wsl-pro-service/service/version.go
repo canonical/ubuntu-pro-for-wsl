e6e5c52a0114a9b04f9c8f94395d902eff705310
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2025-02-25 09:56:02 -0300

    Fix description of wsl-pro-service version command

--- wsl-pro-service/cmd/wsl-pro-service/service/version.go
+++ wsl-pro-service/cmd/wsl-pro-service/service/version.go
@@ -11,7 +11,7 @@ import (
 func (a *App) installVersion() {
 	cmd := &cobra.Command{
 		Use:   "version",
-		Short: i18n.G("Returns version of agent and exits"),
+		Short: i18n.G("Returns version of wsl-pro-service and exits"),
 		Args:  cobra.NoArgs,
 		RunE:  func(cmd *cobra.Command, args []string) error { return getVersion() },
 	}
