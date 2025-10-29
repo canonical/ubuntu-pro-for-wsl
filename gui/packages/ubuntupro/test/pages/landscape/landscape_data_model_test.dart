4be252249fba9d429e36e14d03271872ff776915
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2025-10-29 18:25:47 -0300

    Modernizes the Landscape tests to use golden files

--- gui/packages/ubuntupro/test/pages/landscape/landscape_data_model_test.dart
+++ gui/packages/ubuntupro/test/pages/landscape/landscape_data_model_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ini/ini.dart';
-
 import 'package:ubuntupro/pages/landscape/landscape_model.dart';
 
+import '../../utils/golden.dart';
 import 'constants.dart';
 
 // Records are great when fields have no meaningful default values.
@@ -18,7 +17,6 @@ class _ManualTestCase {
   final FileError wantFileError;
   final AccountNameError wantAccountNameError;
   final Matcher wantComplete;
-  final Matcher wantConfig;
 
   const _ManualTestCase({
     // inputs
@@ -32,151 +30,125 @@ class _ManualTestCase {
     this.wantFileError = FileError.none,
     this.wantAccountNameError = AccountNameError.none,
     this.wantComplete = isTrue,
-    required this.wantConfig,
   });
 }
 
 void main() {
   group('manual data model', () {
-    final testcases = <String, _ManualTestCase>{
+    final testcases = const <String, _ManualTestCase>{
       'success': _ManualTestCase(
         fqdn: selfHostedURL,
-        wantConfig: contains(kExampleLandscapeFQDN),
       ),
       'success with localhost': _ManualTestCase(
         fqdn: 'localhost',
-        wantConfig: contains('localhost:6554'),
       ),
       'success with other schemes': _ManualTestCase(
         fqdn: 'magnet://$kExampleLandscapeFQDN',
-        wantConfig: contains('magnet://$kExampleLandscapeFQDN'),
       ),
       'success with raw ipv4': _ManualTestCase(
         fqdn: '192.168.15.13',
-        wantConfig: contains('https://192.168.15.13/message-system'),
       ),
       'success with raw ipv6': _ManualTestCase(
         fqdn: '2001:db8::1',
-        wantConfig: contains('https://[2001:db8::1]/message-system'),
       ),
       'ping_url remains http': _ManualTestCase(
         fqdn: 'magnet://$kExampleLandscapeFQDN',
-        wantConfig: contains('http://$kExampleLandscapeFQDN/ping'),
       ),
       'client url remains as input': _ManualTestCase(
         fqdn: 'magnet://$kExampleLandscapeFQDN',
-        wantConfig: contains('magnet://$kExampleLandscapeFQDN/message-system'),
       ),
       'success with full URL': _ManualTestCase(
         fqdn: 'http://$kExampleLandscapeFQDN:8080',
-        wantConfig: contains('$kExampleLandscapeFQDN:8080'),
       ),
       'success with registration key': _ManualTestCase(
         fqdn: selfHostedURL,
         registrationKey: 'abc',
-        wantConfig: contains(kExampleLandscapeFQDN),
       ),
       'success with valid cert': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: validCert,
-        wantConfig: contains(kExampleLandscapeFQDN),
       ),
       'success with valid cert and key': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: validCert,
         registrationKey: 'abc',
-        wantConfig: contains(kExampleLandscapeFQDN),
       ),
       'success changing cert into empty path': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: '-',
         registrationKey: 'abc',
-        wantConfig: contains(kExampleLandscapeFQDN),
       ),
       'success with Landscape docs': _ManualTestCase(
         fqdn: 'landscape-server.domain.com:6555',
-        wantConfig: contains('6555'),
       ),
-      'error with unintended URIs': const _ManualTestCase(
+      'error with unintended URIs': _ManualTestCase(
         // This looks like 'host:port' but it's `scheme:path` and we cannot fix it because the path is not an integer.
         fqdn: 'landscape-server.domain.com:6555/',
         wantFQDNError: FqdnError.invalid,
         wantFileError: FileError.none,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with SaaS landscape and no account name': const _ManualTestCase(
+      'error with SaaS landscape and no account name': _ManualTestCase(
         fqdn: saasURL,
         accountName: '',
         wantAccountNameError: AccountNameError.invalid,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with SaaS landscape and standalone account name':
-          const _ManualTestCase(
+      'error with SaaS landscape and standalone account name': _ManualTestCase(
         fqdn: saasURL,
         accountName: standaloneAN,
         wantAccountNameError: AccountNameError.invalid,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
       'success with SaaS landscape and account name': _ManualTestCase(
         fqdn: saasURL,
         accountName: 'user',
-        wantConfig: contains('user'),
       ),
       'self-hosted rejects not standalone account': _ManualTestCase(
         fqdn: kExampleLandscapeFQDN,
         accountName: 'user',
         wantAccountNameError: AccountNameError.none,
         wantComplete: isTrue,
-        wantConfig: contains('account_name = standalone'),
       ),
-      'error with invalid fqdn': const _ManualTestCase(
+      'error with invalid fqdn': _ManualTestCase(
         fqdn: ':::',
         certPath: validCert,
         registrationKey: 'abc',
         wantFQDNError: FqdnError.invalid,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error due numbers only not being a valid URI': const _ManualTestCase(
+      'error due numbers only not being a valid URI': _ManualTestCase(
         fqdn: '12345:6789',
         wantFQDNError: FqdnError.invalid,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with not found cert': const _ManualTestCase(
+      'error with not found cert': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: notFoundPath,
         registrationKey: 'abc',
         wantFileError: FileError.notFound,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with invalid cert': const _ManualTestCase(
+      'error with invalid cert': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: invalidCert,
         registrationKey: 'abc',
         wantFileError: FileError.invalidFormat,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with cert path as a dir': const _ManualTestCase(
+      'error with cert path as a dir': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: './test',
         registrationKey: 'abc',
         wantFileError: FileError.dir,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
-      'error with empty cert': const _ManualTestCase(
+      'error with empty cert': _ManualTestCase(
         fqdn: selfHostedURL,
         certPath: emptyFile,
         registrationKey: 'abc',
         wantFileError: FileError.emptyFile,
         wantComplete: isFalse,
-        wantConfig: isNull,
       ),
     };
     for (final MapEntry(key: name, value: tc) in testcases.entries) {
@@ -198,10 +170,13 @@ void main() {
         expect(c.fileError, tc.wantFileError);
         expect(c.accountNameError, tc.wantAccountNameError);
         expect(c.isComplete, tc.wantComplete);
-        final raw = c.config();
-        expect(raw, tc.wantConfig);
-        if (raw != null) {
-          expectINI(raw);
+        if (tc.wantComplete == isTrue) {
+          expectGoldenIni(
+            'manual_data_model',
+            name,
+            'landscape.conf',
+            c.config()!,
+          );
         }
       });
     }
@@ -264,23 +239,6 @@ void main() {
       });
     }
   });
-}
-
-void expectINI(String raw) {
-  final config = Config.fromStrings(raw.split('\n'));
-  expectNoEmptyValuesInINI(config);
-  expectUrlSchemes(config);
-}
-
-void expectNoEmptyValuesInINI(Config config) {
-  for (final o in config.items('client')!) {
-    expect(o[1], isNotEmpty);
-  }
-}
-
-void expectUrlSchemes(Config config) {
-  final ping = Uri.parse(config.get('client', 'ping_url')!);
-  expect(ping.scheme, 'http');
 }
 
 const saasURL = 'https://landscape.canonical.com';
