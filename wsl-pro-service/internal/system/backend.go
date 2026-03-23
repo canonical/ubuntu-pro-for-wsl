d6208aab4c7691aa149c19aac379ffe27a5fab4d
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2026-03-23 08:39:16 -0300

    Avoid file level nolint
    While convenient that may facilitate security regressions in the future.

--- wsl-pro-service/internal/system/backend.go
+++ wsl-pro-service/internal/system/backend.go
@@ -1,4 +1,3 @@
-//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 package system
 
 import (
@@ -27,24 +26,29 @@ func (b realBackend) GetenvWslDistroName() string {
 
 // ProExecutable returns the full command to run the pro executable with the provided arguments.
 func (b realBackend) ProExecutable(ctx context.Context, args ...string) *exec.Cmd {
+	//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 	return exec.CommandContext(ctx, "pro", args...)
 }
 
 func (b realBackend) LandscapeConfigExecutable(ctx context.Context, args ...string) *exec.Cmd {
+	//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 	return exec.CommandContext(ctx, "landscape-config", args...)
 }
 
 // ProExecutable returns the full command to run the wslpath executable with the provided arguments.
 func (b realBackend) WslpathExecutable(ctx context.Context, args ...string) *exec.Cmd {
+	//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 	return exec.CommandContext(ctx, "wslpath", args...)
 }
 
 // WslinfoExecutable returns the full command to run the wslinfo executable with the provided arguments.
 func (b realBackend) WslinfoExecutable(ctx context.Context, args ...string) *exec.Cmd {
+	//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 	return exec.CommandContext(ctx, "wslinfo", args...)
 }
 
 func (b realBackend) CmdExe(ctx context.Context, path string, args ...string) *exec.Cmd {
+	//nolint:gosec // G204 // We control the input variables, there is no risk of command injection.
 	cmd := exec.CommandContext(ctx, path, args...)
 
 	// cmd.exe must run within the Windows filesystem to avoid warnings.
