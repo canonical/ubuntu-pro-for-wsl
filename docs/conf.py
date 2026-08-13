618887f4e9e60dfcc9d0314a454b44c4f3f49c2c
shanecrowley <shane.crowley@canonical.com>
2026-08-13 10:04:21 +0100

    migrate to Sphinx Stack version 2.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-import yaml
+import textwrap
 
 # Configuration for the Sphinx documentation builder.
 # All configuration specific to your project should be done in this file.
@@ -11,10 +11,9 @@ import yaml
 # A complete list of built-in Sphinx configuration values:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
-# Our starter pack uses the custom Canonical Sphinx extension
-# to keep all documentation based on it consistent and on brand:
+# The Sphinx Stack uses the Canonical Sphinx theme to keep all documentation consistent
+# and on brand:
 # https://github.com/canonical/canonical-sphinx
-
 
 #######################
 # Project information #
@@ -22,50 +21,21 @@ import yaml
 
 # Project name
 #
-# TODO: Update with the official name of your project or product
+# Update with the official name of your project or product
 
 project = "Ubuntu on WSL"
 author = "Canonical Ltd."
 
+# The year in the copyright statement
+copyright = f"{datetime.date.today().year}"
 
-# Sidebar documentation title; best kept reasonably short
-#
-# TODO: To include a version number, add it here (hardcoded or automated).
-#
-# TODO: To disable the title, set to an empty string.
-
+# Sidebar documentation title
+# To disable the title, set it to an empty string.
 html_title = project + " documentation"
-
-
-# Copyright string; shown at the bottom of the page
-#
-# Now, the starter pack uses CC-BY-SA as the license
-# and the current year as the copyright year.
-#
-# TODO: If your docs need another license, specify it instead of 'CC-BY-SA'.
-#
-# TODO: If your documentation is a part of the code repository of your project,
-#       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
-#
-# NOTE: For static works, it is common to provide the first publication year.
-#       Another option is to provide both the first year of publication
-#       and the current year, especially for docs that frequently change,
-#       e.g. 2022–2023 (note the en-dash).
-#
-#       A way to check a repo's creation date is to get a classic GitHub token
-#       with 'repo' permissions; see https://github.com/settings/tokens
-#       Next, use 'curl' and 'jq' to extract the date from the API's output:
-#
-#       curl -H 'Authorization: token <TOKEN>' \
-#         -H 'Accept: application/vnd.github.v3.raw' \
-#         https://api.github.com/repos/canonical/<REPO> | jq '.created_at'
-
-copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
-
 
 # Documentation website URL
 #
-# TODO: Update with the official URL of your docs or leave empty if unsure.
+# Update with the official URL of your docs or leave empty if unsure.
 #
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
@@ -76,90 +46,77 @@ ogp_site_url = f"https://ubuntu.com/wsl/docs/{version}"
 
 
 # Preview name of the documentation website
-#
-# TODO: To use a different name for the project in previews, update as needed.
-
+# To use a different name for the project in previews, update the next line.
 ogp_site_name = project
 
-
 # Preview image URL
-#
-# TODO: To customise the preview image, update as needed.
-
+# To customise the preview image, update the next line.
 ogp_image = "https://assets.ubuntu.com/v1/cc828679-docs_illustration.svg"
-
 
 # Product favicon; shown in bookmarks, browser tabs, etc.
 
-# TODO: To customise the favicon, uncomment and update as needed.
+# To customise the favicon, uncomment and update as needed.
 
 # html_favicon = '.sphinx/_static/favicon.png'
 
 
 # Dictionary of values to pass into the Sphinx context for all pages:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_context
-
 html_context = {
     # Product page URL; can be different from product docs URL
     #
-    # TODO: Change to your product website URL,
+    # Change to your product website URL,
     #       dropping the 'https://' prefix, e.g. 'ubuntu.com/lxd'.
     #
-    # TODO: If there's no such website,
+    # If there's no such website,
     #       remove the {{ product_page }} link from the page header template
     #       (usually .sphinx/_templates/header.html; also, see README.rst).
     "product_page": "ubuntu.com/wsl",
     # Product tag image; the orange part of your logo, shown in the page header
-    #
-    # TODO: To add a tag image, uncomment and update as needed.
+    # To add a tag image, uncomment and update as needed.
     # 'product_tag': '_static/tag.png',
     # Your Discourse instance URL
-    #
-    # TODO: Change to your Discourse instance URL or leave empty.
+    # Change to your Discourse instance URL or leave empty.
     #
     # NOTE: If set, adding ':discourse: 123' to an .rst file
     #       will add a link to Discourse topic 123 at the bottom of the page.
     "discourse": "https://discourse.ubuntu.com/c/wsl/27",
     # Your Mattermost channel URL
-    #
-    # TODO: Change to your Mattermost channel URL or leave empty.
+    # Change to your Mattermost channel URL or leave empty.
     "mattermost": "",
     # Your Matrix channel URL
-    #
-    # TODO: Change to your Matrix channel URL or leave empty.
+    # Change to your Matrix channel URL or leave empty.
     "matrix": "https://matrix.to/#/#ubuntu-wsl:ubuntu.com",
     # Your documentation GitHub repository URL
     #
-    # TODO: Change to your documentation GitHub repository URL or leave empty.
+    # Change to your documentation GitHub repository URL or leave empty.
     #
     # NOTE: If set, links for viewing the documentation source files
     #       and creating GitHub issues are added at the bottom of each page.
     "github_url": "https://github.com/canonical/ubuntu-pro-for-wsl",
     # Docs branch in the repo; used in links for viewing the source files
-    #
-    # TODO: To customise the branch, uncomment and update as needed.
     "repo_default_branch": "main",
     # Docs location in the repo; used in links for viewing the source files
-    #
-    # TODO: To customise the directory, uncomment and update as needed.
     "repo_folder": "/docs/",
-    # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
+    # To enable or disable the Previous / Next buttons at the bottom of pages
     # Valid options: none, prev, next, both
     "sequential_nav": "both",
-    # TODO: To enable listing contributors on individual pages, set to True
-    "display_contributors": True,
+    # To enable listing contributors on individual pages, set to True
+    "display_contributors": False, # deprecated
     # Required for feedback button
     "github_issues": "enabled",
+    "author": author,
+    "license": {
+        # Specify your project's license.
+        # For the name, we recommend using the standard shorthand identifier from
+        # https://spdx.org/licenses
+        "name": "CC-BY-SA", # NOTE: using as the docs license
+        # Link directly to your project's license statement.
+        "url": "https://creativecommons.org/licenses/by-sa/4.0/deed.en",
+    },
 }
 
-html_extra_path = []
-
-# Allow opt-in build of the OpenAPI "Hello" example so docs stay clean by default.
-if os.getenv("OPENAPI", ""):
-    tags.add("openapi")
-    html_extra_path.append("how-to/assets/openapi.yaml")
-
-# TODO: To enable the edit button on pages, uncomment and change the link to a
+# To enable the edit button on pages, uncomment and change the link to a
 # public repository on GitHub or Launchpad. Any of the following link domains
 # are accepted:
 # - https://github.com/example-org/example"
@@ -169,10 +126,11 @@ if os.getenv("OPENAPI", ""):
 # html_theme_options = {
 #     "source_edit_link": "https://github.com/canonical/ubuntu-pro-for-wsl",
 # }
+# NOTE: not enabling edit button because it doesn't handle versioned docs
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
 #
-# TODO: If your documentation is hosted on https://docs.ubuntu.com/,
+# If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
 slug = "wsl/docs"
@@ -201,67 +159,54 @@ sitemap_excludes = [
     'search/',
 ]
 
-# TODO: Add more pages to sitemap_excludes if needed. Wildcards are supported.
+# Add more pages to sitemap_excludes if needed. Wildcards are supported.
 #       For example, to exclude module pages generated by autodoc, add '_modules/*'.
 
-#######################
-# Template and asset locations
-#######################
+################################
+# Template and asset locations #
+################################
 
-html_static_path = [".sphinx/_static"]
-templates_path = [".sphinx/_templates"]
+html_static_path = ["_static"]
+templates_path = ["_templates"]
 
 
 #############
 # Redirects #
 #############
 
-# To set up redirects: https://documatt.gitlab.io/sphinx-reredirects/usage.html
-# For example: 'explanation/old-name.html': '../how-to/prettify.html',
+# Add redirects to the 'redirects.txt' file
+# https://sphinxext-rediraffe.readthedocs.io/en/latest/
 
 # To set up redirects in the Read the Docs project dashboard:
 # https://docs.readthedocs.io/en/stable/guides/redirects.html
 
-# NOTE: If undefined, set to None, or empty,
-#       the sphinx_reredirects extension will be disabled.
+# To set up redirects when a page has moved or been renamed, use the redirects.txt file
+rediraffe_redirects = "redirects.txt"
 
-redirects = {
-    # deprecated tutorials
-    "tutorials/dotnet-systemd": "../../",
-    "tutorials/interop": "../../",
-    # renamed tutorials
-    "tutorials/vscode": "../../tutorials/develop-with-ubuntu-wsl",
-    # tutorials that have converted to howto guides
-    "tutorials/cloud-init": "../../howto/cloud-init",
-    "tutorials/data-science-and-engineering": "../../howto/data-science-and-engineering",
-    "tutorials/gpu-cuda": "../../howto/gpu-cuda",
-    "tutorials/deployment": "../../howto/deploy-with-landscape-web-portal",
-    # change in diataxis names
-    "guides/": "../../howto/",
-    "guides/contributing": "../../howto/contributing",
-    "guides/install-ubuntu-wsl2": "../../howto/install-ubuntu-wsl2",
-    "guides/run-workflows-azure": "../../howto/run-workflows-azure",
-    "explanations/": "../../explanation/",
-    "explanations/ref-arch-explanation": "../../explanation/ref-arch-explanation",
-    # improved url to account for merging of distro and UP4W app docs
-    "tutorials/getting-started": "../../tutorials/getting-started-with-up4w",
-    # account for old use of "tutorial/"
-    "tutorial/": "../../tutorials/",
-    "tutorial/getting-started": "../../tutorials/getting-started-with-up4w",
-    "tutorial/deployment": "../../tutorials/deployment",
-    # ... even for URLs that never existed
-    "tutorial/getting-started-with-up4w": "../../tutorials/getting-started-with-up4w",
-    "tutorial/develop-with-ubuntu-wsl": "../../tutorials/develop-with-ubuntu-wsl",
-    # old UP4W explainer is redundant so redirect to arch explanation
-    "explanations/up4w": "../../explanation/ref-arch-explanation",
-    # deprecated feature so point users to relevant doc
-    "guides/autoinstall": "../../howto/cloud-init",
-    "reference/glossary_up4w_components": "../../reference/glossary",
-    # expanded guide to cover more general topics
-    "howto/backup-and-restore": "../../howto/manage-and-configure",
-    # we can point to the RTD-based release notes for Ubuntu
-    "reference/release_notes": "https://documentation.ubuntu.com/release-notes/",
-}
+# NOTE: for automated checking of changed/moved URLS --- note enabled (yet)
+# rediraffe_branch = "main"
+
+# Strips '/index.html' from destination URLs when building with 'dirhtml'
+rediraffe_dir_only = True
+
+
+############################
+# sphinx-llm configuration #
+############################
+
+# This description is included in llms.txt to provide some initial context for your
+# product docs.
+# Add a description in the form "This is the documentation for <product name>,
+# <first sentence of home page>".
+llms_txt_description = textwrap.dedent(
+    """\
+    This is the documentation for Ubuntu on WSL, the default Linux distribution for WSL, and the Pro for WSL application for managing and securing instances of Ubuntu on WSL.
+    """
+)
+
+# The base URL for references built by sphinx-markdown-builder.
+if os.environ.get("READTHEDOCS"):
+    markdown_http_base = html_baseurl
 
 
 ###########################
@@ -269,9 +214,6 @@ redirects = {
 ###########################
 
 # A regex list of URLs that are ignored by 'make linkcheck'
-#
-# TODO: Remove or adjust the ACME entry after you update the contributing guide
-
 linkcheck_ignore = [
     "http://127.0.0.1:8000",
     # Linkcheck does not have access to the repo
@@ -288,7 +230,6 @@ linkcheck_ignore = [
 
 
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
-
 linkcheck_anchors_ignore_for_url = [
     r"https://github\.com/.*",
     r"https://learn.microsoft\.com/.*",
@@ -304,28 +245,26 @@ linkcheck_retries = 3
 
 # Custom MyST syntax extensions; see
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
-#
 # NOTE: By default, the following MyST extensions are enabled:
-#       substitution, deflist, linkify
-
+#   - substitution
+#   - deflist
+#   - linkify
 myst_enable_extensions = set({"colon_fence"})
 
 # Custom Sphinx extensions; see
 # https://www.sphinx-doc.org/en/master/usage/extensions/index.html
-
-# NOTE: The canonical_sphinx extension is required for the starter pack.
-
 extensions = [
     "canonical_sphinx",
     "notfound.extension",
     "sphinx_design",
-    "sphinx_reredirects",
+    "sphinx_rerediraffe",
     "sphinx_tabs.tabs",
     "sphinxcontrib.jquery",
     "sphinxext.opengraph",
     "sphinx_config_options",
     "sphinx_contributor_listing",
     "sphinx_filtered_toctree",
+    "sphinx_llm.txt",
     "sphinx_related_links",
     "sphinx_roles",
     "sphinx_terminal",
@@ -338,59 +277,46 @@ extensions = [
     "sphinxcontrib.mermaid",
 ]
 
-# Use a newer than default mermaid version.
-# 11.13.0 fixes quoted participant names with entity type annotations
-# (https://github.com/mermaid-js/mermaid/pull/7136).
-# mermaid_version = "11.15.0"
-
 # Excludes files or directories from processing
-
 exclude_patterns = [
+    ".venv*",
 ]
 
-# Adds custom CSS files, located under 'html_static_path'
+# Adds custom CSS files, located under 'html_static_path' or remotely
+html_css_files = [
+        "css/pro_block.css",
+        "css/dropdown.css",
+        "css/mermaid-custom.css"
+        "https://assets.ubuntu.com/v1/d86746ef-cookie_banner.css",
+        ]
 
-html_css_files = ["css/pro_block.css", "css/cookie-banner.css", "css/dropdown.css", "css/mermaid-custom.css"]
 
-
-# Adds custom JavaScript files, located under 'html_static_path'
-
+# Adds custom JavaScript files, located under 'html_static_path' or remotely
 html_js_files = [
-        "js/bundle.js",
+        "https://assets.ubuntu.com/v1/287a5e8f-bundle.js",
 ]
 
-
-# Specifies a reST snippet to be appended to each .rst file
-
-rst_epilog = """
-.. include:: /reuse/links.txt
-.. include:: /reuse/substitutions.txt
-"""
+# Appends extra markup to the end of every document written in reST
+# rst_epilog = """
+# """
 
 # Feedback button at the top; enabled by default
-#
-# TODO: To disable the button, uncomment this.
-
+# Disable the button if your project is unsuitable for public feedback.
 # disable_feedback_button = True
 
-
 # Your manpage URL
-#
-# TODO: To enable manpage links, uncomment and replace {codename} with required
+# To enable manpage links, uncomment and replace {codename} with required
 #       release, preferably an LTS release (e.g. noble). Do *not* substitute
 #       {section} or {page}; these will be replaced by sphinx at build time
 #
 # NOTE: If set, adding ':manpage:' to an .rst file
 #       adds a link to the corresponding man section at the bottom of the page.
-
 # manpages_url = 'https://manpages.ubuntu.com/manpages/{codename}/en/' + \
 #     'man{section}/{page}.{section}.html'
-
 
 # Specifies a reST snippet to be prepended to each .rst file
 # This defines a :center: role that centers table cell content.
 # This defines a :h2: role that styles content for use with PDF generation.
-
 rst_prolog = """
 .. role:: center
    :class: align-center
@@ -407,22 +333,16 @@ rst_prolog = """
 if "discourse_prefix" not in html_context and "discourse" in html_context:
     html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
 
-# Workaround for substitutions.yaml
-
-if os.path.exists("./reuse/substitutions.yaml"):
-    with open("./reuse/substitutions.yaml", "r") as fd:
-        myst_substitutions = yaml.safe_load(fd.read())
-
-# Add configuration for intersphinx mapping
-
-intersphinx_mapping = {
-    'starter-pack': ('https://canonical-example-product-documentation.readthedocs-hosted.com/en/latest', None),
-    'sphinxcontrib-mermaid': ('https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest', None)
-}
+# Configuration for Intersphinx projects
+#
+# intersphinx_mapping = {
+#     "snap": ("https://snapcraft.io/docs/", None),
+# }
 
 # Define a selector that only adds copy buttons to code blocks without the class `no-copy`
 copybutton_selector = "div:not(.no-copy) > div.highlight > pre"
 
+# TODO: deprecate in favor of terminal extension
 # Define prompts to be excluded from copying when a copy button is used
 copybutton_prompt_text = r"^.*?[\$>]\s+"
 copybutton_prompt_is_regexp = True
